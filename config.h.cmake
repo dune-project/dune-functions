c2282bb2862c98e51fbeddc553b92acf54d6d425
Steffen Müthing <muething@dune-project.org>
2013-08-20 10:24:16 +0200

    Initial setup of dune-functions module

---
+++ config.h.cmake
@@ -0,0 +1,45 @@
+/* begin functions
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten 
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of dune-functions */
+#define Udune_functions_VERSION "@Udune_functions_VERSION@"
+
+/* Define to the major version of dune-functions */
+#define Udune_functions_VERSION_MAJOR @Udune_functions_VERSION_MAJOR@
+
+/* Define to the minor version of dune-functions */
+#define Udune_functions_VERSION_MINOR @Udune_functions_VERSION_MINOR@
+
+/* Define to the revision of dune-functions */
+#define Udune_functions_VERSION_REVISION @Udune_functions_VERSION_REVISION@
+
+/* end functions
+   Everything below here will be overwritten
+*/
---
+++ dune/functions/functions.hh
@@ -0,0 +1,6 @@
+#ifndef DUNE_functions.hh
+#define DUNE_functions.hh
+
+// add your classes here
+
+#endif // DUNE_functions.hh
