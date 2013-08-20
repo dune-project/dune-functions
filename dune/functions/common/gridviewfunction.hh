dddc787ae3c24381b5f3eb4215161da86d0bcdd0
Steffen Müthing <muething@dune-project.org>
2013-08-20 17:54:16 +0200

    [GridViewFunction] Next iteration of discrete grid function interface

--- dune/functions/common/gridviewfunction.hh
+++ dune/functions/common/gridviewfunction.hh
@@ -10,6 +10,45 @@ namespace Dune {
 
 namespace Functions {
 
+  /**
+   * \tparam DF Type derived from DifferentiableFunction
+   * \tparam LC  Type of local context, e.g. grid cell
+   */
+template<typename DF, typename LC>
+class LocalFunction
+  : public DifferentiableFunction<typename LC::Geometry::LocalCoordinate, typename DF::Range>
+{
+
+  typedef DF GlobalFunction;
+  typedef LC LocalContext;
+
+  typedef typename LC::Geometry::LocalCoordinate Domain;
+
+  typedef LocalFunction<Domain,typename DF::DerivativeRange> Derivative;
+
+  virtual void bind(const LocalContext&) = 0;
+
+  virtual Derivative* derivative() = 0;
+
+  virtual const LocalContext& localContext() = 0;
+
+};
+
+
+template<typename Function>
+shared_ptr<typename Function::ElementFunction> elementFunction(const Function& f)
+{
+  return static_pointer_cast<typename Function::ElementFunction>(f.elementFunction());
+}
+
+template<typename Function>
+shared_ptr<typename Function::ElementFunction> elementFunction(const Function& f, const typename Function::Element& e)
+{
+  auto p = static_pointer_cast<typename Function::ElementFunction>(f.elementFunction());
+  p.bind(e);
+  return std::move(p);
+}
+
 
 
 template<typename GV, typename RT>
@@ -29,15 +68,81 @@ class GridViewFunction
 
   typedef GridViewFunction<GV,DerivativeRange> Derivative;
 
-
   typedef typename GV::template Codim<0>::Geometry::LocalCoordinate LocalDomain;
   typedef typename GV::template Codim<0>::Entity Element;
 
-  virtual void evaluate(const Element& e, const LocalDomain& coord, Range& r) const = 0;
+  typedef LocalFunction<GridViewFunction,Element> ElementFunction;
+
+protected:
+
+  typedef shared_ptr<ElementFunction> ElementFunctionBasePointer;
+
+public:
+
+  virtual ElementFunctionBasePointer elementFunction() const = 0;
+
+  // virtual void evaluate(const Element& e, const LocalDomain& coord, Range& r) const
 
   virtual Derivative* derivative() const = 0;
 
+  const GridView& gridView()
+  {
+    return gv_;
+  }
+
+  GridViewFunction(const GridView& gv)
+    : gv_(gv)
+  {}
+
+private:
+
+  GridView gv_;
+
 };
+
+
+
+template<typename GFS, typename V>
+class DiscreteGridViewFunction
+  : public GridViewFunction<typename GFS::GlobalCoordinate,
+                            typename GFS::Range
+                            >
+{
+
+  class ElementFunction
+    : public
+  {
+
+    Eleemn
+
+    virtual void evaluate(const Element& e, const LocalDomain& coord, Range& r)
+    {
+      lfs.bind(e);
+      lfs_cache.update();
+      x_view.bind(lfs_cache);
+      x_view.read(xl);
+      x_view.unbind();
+      lfs.finiteElement().basis().evaluateFunction(coord,yb);
+      r = 0;
+      for (unsigned int i=0; i<yb.size(); i++)
+        {
+          r.axpy(xl[i],yb[i]);
+        }
+    }
+
+  };
+
+  virtual typename Base::ElementFunctionBasePointer elementFunction() const
+  {
+    return make_shared<ElementFunction>(...);
+  }
+
+};
+
+
+
+
+
 
 
 
