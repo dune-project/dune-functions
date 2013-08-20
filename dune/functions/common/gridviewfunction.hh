fc26a687c1c760af9bab1f5e727cae8052e2a6f1
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-08-20 18:47:11 +0200

    put class GridViewFunction into a separate file

--- dune/functions/common/gridviewfunction.hh
+++ dune/functions/common/gridviewfunction.hh
@@ -52,57 +52,6 @@ shared_ptr<typename Function::ElementFunction> elementFunction(const Function& f
 }
 
 
-
-template<typename GV, typename RT>
-class GridViewFunction
-  : public DifferentiableFunction<typename GV::template Codim<0>::Geometry::GlobalCoordinate,RT>
-{
-
-  typedef DifferentiableFunction<
-    typename GV::template Codim<0>::Geometry::GlobalCoordinate,
-    RT
-    > Base;
-
-  typedef GV GridView;
-  typedef typename Base::Domain Domain;
-  typedef typename Base::Range Range;
-  typedef typename Base::DerivativeRange DerivativeRange;
-
-  typedef GridViewFunction<GV,DerivativeRange> Derivative;
-
-  typedef typename GV::template Codim<0>::Geometry::LocalCoordinate LocalDomain;
-  typedef typename GV::template Codim<0>::Entity Element;
-
-  typedef LocalFunction<GridViewFunction,Element> ElementFunction;
-
-protected:
-
-  typedef shared_ptr<ElementFunction> ElementFunctionBasePointer;
-
-public:
-
-  virtual ElementFunctionBasePointer elementFunction() const = 0;
-
-  // virtual void evaluate(const Element& e, const LocalDomain& coord, Range& r) const
-
-  virtual Derivative* derivative() const = 0;
-
-  const GridView& gridView()
-  {
-    return gv_;
-  }
-
-  GridViewFunction(const GridView& gv)
-    : gv_(gv)
-  {}
-
-private:
-
-  GridView gv_;
-
-};
-
-
 } // end of namespace Dune::Functions
 } // end of namespace Dune
 
