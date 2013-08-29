0986f75279ca74c411990d6354d13ecd9c8a688b
Carsten Gräser <graeser@dune-project.org>
2013-08-29 12:33:38 +0200

    Add class FunctionFromCallable<D,R>
    This class implement DifferentiableFunction<D,R> using
    callable objects. Any type that can be assigned to
    std::function<D,R> is supported. By providing more
    then one callable you can also implement a variable
    number of derivatives. (Variadic templates rock !)

---
+++ dune/functions/common/functionfromcallable.hh
@@ -0,0 +1,145 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_COMMON_FUNCTION_FROM_CALLABLE_HH
+#define DUNE_FUNCTIONS_COMMON_FUNCTION_FROM_CALLABLE_HH
+
+
+namespace Dune {
+namespace Functions {
+
+
+template<class D, class R>
+class FunctionFromCallable;
+
+// Helper class that exports the type for FunctionFromCallable<D,R>::Derivative
+// and constructs the corresponding object is possible.
+// Notice that the second template argument is the DerivativeRange and NOT the Range
+template<class D, class DR>
+struct FunctionFromCallableDerivativeTraits
+{
+  typedef FunctionFromCallable<D, DR> Derivative;
+
+  template<class... DF>
+  static Dune::shared_ptr<Derivative> makeIfImplemented(const DF&... df)
+  {
+    return Dune::make_shared<Derivative>(df...);
+  }
+};
+
+// Specialization for DerivativeRange==InvalidRange. In this case
+// FunctionFromCallable<D,R>::Derivative is the interface class.
+template<class D>
+struct FunctionFromCallableDerivativeTraits<D, InvalidRange>
+{
+  typedef DifferentiableFunction<D, InvalidRange> Derivative;
+
+  template<class... DF>
+  static Dune::shared_ptr<Derivative> makeIfImplemented(const DF&... df)
+  {
+    DUNE_THROW(Dune::NotImplemented, "DerivativeRange is not implemented for this type.");
+  }
+};
+
+
+
+/**
+ * \brief Wrap a callable object as Dune::DifferentiableFunction
+ *
+ * You can use this to implement a DifferentiableFunction including
+ * a variable number of derivatives using callable objects. All
+ * types that can be assigned to std::function are supported,
+ * i.e. functions, functors, lambdas, ...
+ *
+ * \tparam D The domain type
+ * \tparam R The range type
+ *
+ */
+template<class D, class R>
+class FunctionFromCallable :
+  public DifferentiableFunction<D, R>
+{
+    typedef DifferentiableFunction<D, R> Base;
+
+  public:
+
+    typedef typename Base::Range Range;
+    typedef typename Base::Domain Domain;
+    typedef typename Base::DerivativeRange DerivativeRange;
+
+    typedef typename FunctionFromCallableDerivativeTraits<Domain, DerivativeRange>::Derivative Derivative;
+
+    /**
+     * \brief Create function from callable object
+     *
+     * This will store a std::function<Domain,Range> wrapping the callable
+     * and pass evaluate() to this object.
+     *
+     * \tparam F Anything that can be assigned to std::function<Domain, Range> (funcions, functors, lambdas,...)
+     * \param f Callable object to use for evaluate()
+     */
+    template<typename F>
+    FunctionFromCallable(const F& f)
+    {
+      f_ = f;
+    }
+
+    /**
+     * \brief Create function and derivatives from callable objects
+     *
+     * This will store a std::function<Domain,Range> wrapping the callable
+     * and pass evaluate() to this object.
+     *
+     * All further arguments are used to implement derivatives.
+     * The n-th argument should be assignable to std::function<Domain, NDR>
+     * with NDR being the Range of the n-th derivative.
+     * By passing n arguments the created function will support
+     * the evaluation of n derivatives.
+     *
+     * \tparam F Anything that can be assigned to std::function<Domain, Range> (funcions, functors, lambdas,...)
+     * \tparam DF Variable length arg list for derivatives
+     *
+     * \param f Callable object to use for evaluate()
+     * \param df Variable length arg list of Callable objects to use for evaluate() of derivatives
+     */
+    template<typename F, typename... DF>
+    FunctionFromCallable(const F& f, const DF&... df)
+    {
+      f_ = f;
+      derivative_ = FunctionFromCallableDerivativeTraits<Domain, DerivativeRange>::makeIfImplemented(df...);
+    }
+
+    /**
+     * \brief Evaluate function
+     *
+     * This call is passed to the stored std::function
+     */
+    virtual void evaluate(const Domain& x, Range&y) const DUNE_FINAL
+    {
+      y = f_(x);
+    }
+
+    /**
+     * \brief Return derivative
+     *
+     * If a proper number of derivatives where provided
+     * this will return the derivative as FunctionFromCallable<Domain, DerivativeRange>
+     */
+    virtual Derivative* derivative() const DUNE_FINAL
+    {
+      if (not derivative_)
+        DUNE_THROW(Dune::NotImplemented, "This derivative was not provided.");
+      return derivative_.get();
+    }
+
+  private:
+    std::function<R(D)> f_;
+    Dune::shared_ptr<Derivative> derivative_;
+};
+
+
+
+
+} // namespace Functions
+} // namespace Dune
+
+#endif //DUNE_FUNCTIONS_COMMON_FUNCTION_FROM_CALLABLE_HH
