6325a321d7d9fe60f0ac80ab916798cb92fda7f5
Carsten Gräser <graeser@dune-project.org>
2014-03-18 16:30:43 +0100

    Implement local view and single leaf node tree

--- dune/functions/functionspacebases/pq1functionspacebasis.hh
+++ dune/functions/functionspacebases/pq1functionspacebasis.hh
@@ -4,9 +4,17 @@
 #define DUNE_FUNCTIONS_FUNCTIONSPACEBASES_PQ1FUNCTIONSPACEBASIS_HH
 
 #include <dune/common/reservedvector.hh>
+#include <dune/common/exceptions.hh>
+
+#include <dune/localfunctions/lagrange/pqkfactory.hh>
+
+
 
 template<typename GV>
 class PQ1FunctionSpaceBasisLocalView;
+
+template<typename GV>
+class PQ1FunctionSpaceBasisLeafNode;
 
 //template<typename T>
 //struct FunctionSpaceBasisTraits;
@@ -77,78 +85,152 @@ template<typename GV>
 class PQ1FunctionSpaceBasisLocalView
 {
 public:
+
   typedef PQ1FunctionSpaceBasis<GV> GlobalBasis;
   typedef typename GlobalBasis::GridView GridView;
   typedef typename GlobalBasis::size_type size_type;
   typedef typename GlobalBasis::MultiIndex MultiIndex;
+  typedef typename GridView::template Codim<0>::Entity Element;
+  typedef PQ1FunctionSpaceBasisLeafNode<GV> Tree;
 
   PQ1FunctionSpaceBasisLocalView(const GlobalBasis* globalBasis) :
     globalBasis_(globalBasis)
   {}
 
-protected:
-  const GlobalBasis* globalBasis_;
-};
-
-
-
-#if 0
-
-//! TypeTree node
-template<typename GridViewFunctionSpaceBasis>
-class GridViewLocalBasisView
-{
-  typedef typename GridViewFunctionSpaceBasis::size_type size_type;
-  typedef typename GridViewFunctionSpaceBasis::MultiIndex MultiIndex;
-
   //! Bind to element
-  void bind(const Element& e);
+  void bind(const Element& e)
+  {
+    element_ = &e;
+    tree_.bind(e);
+  }
+
+  const Element& element() const
+  {
+    if (element_)
+      return *element_;
+    else
+      DUNE_THROW(Dune::Exception, "Can't query element of unbound local view");
+  }
 
   //! Unbind from element - only hint
-  void unbind();
+  void unbind()
+  {}
 
   /**
    * \brief Return the local ansatz tree associated to the bound entity
    *
-   * \returns some_type // This is tree
+   * \returns Tree // This is tree
    */
-  some_type& tree() const;
+  const Tree& tree() const
+  {
+    tree_;
+  }
+
+  const GlobalBasis& globalBasis() const
+  {
+    return *globalBasis_;
+  }
+
+protected:
+  const GlobalBasis* globalBasis_;
+  const Element* element_;
+  Tree tree_;
 };
 
 
-template<typename GridViewFunctionSpaceBasis>
-class GridViewLocalBasisViewTreeNode
+template<typename GV>
+class PQ1FunctionSpaceBasisLeafNode
 {
-  typedef typename GridViewFunctionSpaceBasis::size_type size_type;
-  typedef typename GridViewFunctionSpaceBasis::MultiIndex MultiIndex;
+public:
+  typedef PQ1FunctionSpaceBasis<GV> GlobalBasis;
+  typedef typename GlobalBasis::LocalView LocalView;
+  typedef typename GlobalBasis::GridView GridView;
+  typedef typename GlobalBasis::size_type size_type;
+  typedef typename GlobalBasis::MultiIndex MultiIndex;
+  typedef typename GridView::template Codim<0>::Entity Element;
 
-  typedef typename GridViewFunctionSpaceBasis::Element Element;
+  typedef typename Dune::PQkLocalFiniteElementCache<typename GV::Grid::ctype, double, GV::dimension, 1> FiniteElementCache;
+  typedef typename FiniteElementCache::FiniteElementType FiniteElement;
+
+  friend LocalView;
+
+  PQ1FunctionSpaceBasisLeafNode() :
+    finiteElement_(0),
+    element_(0)
+  {}
+
+//  PQ1FunctionSpaceBasisLeafNode(const LocalView& localView) :
+//    localView_(&localView)
+//  {}
 
   //! Return current element, throw if unbound
-  const Element& element() const;
+  const Element& element() const
+  {
+    return *element_;
+  }
+
+  const FiniteElement& finiteElement() const
+  {
+    return *finiteElement_;
+  }
 
   //! size of subtree rooted in this node (element-local)
-//  size_type subTreeSize() const;
+  size_type subTreeSize() const
+  {
+    // We have subTreeSize==lfe.size() because we're in a leaf node.
+    return finiteElement->localBasis().size();
+  }
 
   //! maximum size of subtree rooted in this node for any element of the global basis
-//  size_type maxSubTreeSize() const;
+  size_type maxSubTreeSize() const
+  {
+    return 1<<GridView::dimension;
+  }
 
   //! size of complete tree (element-local)
-  size_type localSize() const;
+  size_type localSize() const
+  {
+    // We have localSize==subTreeSize because the tree consist of a single leaf node.
+    return subTreeSize();
+  }
 
   //! Maps from subtree index set [0..subTreeSize-1] into root index set (element-local) [0..localSize-1]
-  size_type localIndex(size_type i) const;
+  size_type localIndex(size_type i) const
+  {
+    return i;
+  }
 
   //! maximum size of complete tree for any element of the global basis
   size_type maxLocalSize() const;
 
   //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis (pair of multi-indices)
   const MultiIndex& globalIndex(size_type i) const;
+#endif
 
   //! to be discussed
-  const GridViewFunctionSpaceBasis& globalBasis() const;
+//  const GlobalBasis& globalBasis() const
+//  {
+//    return localView_->globalBasis();
+//  }
 
+protected:
+
+  //! Bind to element.
+  void bind(const Element& e)
+  {
+    element_ = &e;
+    finiteElement_ = &(cache_.get(element_->type()));
+  }
+
+//  const LocalView* localView_;
+  FiniteElementCache cache_;
+  const FiniteElement* finiteElement_;
+  const Element* element_;
 };
+
+
+#if 0
+
 
 
 template<typename GridViewFunctionSpaceBasis, typename... Children>
