ee0abebf77ebb4b3245bca6d033b0918beab808a
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-03-18 16:36:49 +0100

    More elaborate test -- integrate over a discrete function

--- dune/functions/functionspacebases/test/gridviewfunctionspacebasistest.cc
+++ dune/functions/functionspacebases/test/gridviewfunctionspacebasistest.cc
@@ -6,6 +6,8 @@
 
 #include <dune/common/exceptions.hh>
 
+#include <dune/geometry/quadraturerules.hh>
+
 #include <dune/grid/yaspgrid.hh>
 
 #include <dune/functions/functionspacebases/pq1functionspacebasis.hh>
@@ -23,12 +25,58 @@ int main (int argc, char* argv[]) try
 
   // Test whether PQ1FunctionSpaceBasis.hh can be instantiated on the leaf view
   typedef GridType::LeafGridView GridView;
-  PQ1FunctionSpaceBasis<GridView> feBasis(grid.leafGridView());
-  auto lv = feBasis.localView();
 
-  auto it = feBasis.gridView().begin<0>();
-  lv.bind(*it);
+  const GridView& gridView = grid.leafGridView();
+  PQ1FunctionSpaceBasis<GridView> feBasis(gridView);
 
+  // Sample the function f(x,y) = x on the grid vertices
+  // If we use that as the coefficients of a finite element function,
+  // we know its integral and can check whether quadrature returns
+  // the correct result
+  std::vector<double> x(0);  // TODO: I don't know the correct size...
+
+  for (auto it = gridView.begin<dim>(); it != gridView.end<dim>(); ++it)
+    x[gridView.indexSet().index(*it)] = it->geometry().corner(0)[0];
+
+  // Loop over elements and integrate over the function
+  double integral = 0;
+  for (auto it = gridView.begin<0>(); it != gridView.end<0>(); ++it)
+  {
+    auto localView = feBasis.localView();
+    localView.bind(*it);
+
+    auto& tree = localView.tree();
+
+    auto& localFiniteElement = tree.finiteElement();
+
+    // A quadrature rule
+    const QuadratureRule<double, dim>& quad = QuadratureRules<double, dim>::rule(it->type(), 1);
+
+    // Loop over all quadrature points
+    for ( size_t pt=0; pt < quad.size(); pt++ ) {
+
+        // Position of the current quadrature point in the reference element
+        const FieldVector<double,dim>& quadPos = quad[pt].position();
+
+        // The multiplicative factor in the integral transformation formula
+        const double integrationElement = it->geometry().integrationElement(quadPos);
+
+        // Evaluate all shape function values at this point
+        std::vector<FieldVector<double,1> > shapeFunctionValues;
+        localFiniteElement.localBasis().evaluateFunction(quadPos, shapeFunctionValues);
+
+        // Actually compute the vector entries
+        for (size_t i=0; i<localFiniteElement.localBasis().size(); i++)
+            integral += localX[i] * shapeFunctionValues[i] * quad[pt].weight() * integrationElement;
+
+    }
+
+
+
+
+  }
+
+  return 0;
 
 } catch ( Dune::Exception &e )
 {
