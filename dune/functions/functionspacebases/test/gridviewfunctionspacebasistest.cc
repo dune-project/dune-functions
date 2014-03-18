62dcbd83328d984d9452926b02d74bc9c082c97f
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-03-18 14:38:44 +0100

    Add a dummy test for the GridViewFunctionSpaceBasis class

---
+++ dune/functions/functionspacebases/test/gridviewfunctionspacebasistest.cc
@@ -0,0 +1,20 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/common/exceptions.hh>
+
+int main (int argc, char* argv[]) try
+{
+  std::cout << "Hello world!" << std::endl;
+
+} catch ( Dune::Exception &e )
+{
+  std::cerr << "Dune reported error: " << e << std::endl;
+}
+catch(...)
+{
+  std::cerr << "Unknown exception thrown!" << std::endl;
+}
