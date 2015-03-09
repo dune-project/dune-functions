11ce680c2eb0a339ca84444483f85dab39967240
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-03-09 22:03:33 +0100

    Add new dir analyticfunctions
    * Add directory
    * Extract Polynomial and TrigonometricFunction into new directory
    
    These implementations may change or vanish again, but for now
    I'll put them here, to use them in multiple tests.

---
+++ dune/functions/analyticfunctions/polynomial.hh
@@ -0,0 +1,71 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_POLYNOMIAL_HH
+#define DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_POLYNOMIAL_HH
+
+
+namespace Dune {
+namespace Functions {
+
+
+template<class K>
+class Polynomial
+{
+public:
+
+  Polynomial()
+  {}
+
+  Polynomial(const Polynomial& other) :
+    coefficients_(other.coefficients_)
+  {}
+
+  Polynomial(Polynomial&& other) :
+    coefficients_(std::move(other.coefficients_))
+  {}
+
+  Polynomial(std::initializer_list<double> coefficients) :
+    coefficients_(coefficients)
+  {}
+
+  Polynomial(std::vector<K>&& coefficients) :
+      coefficients_(std::move(coefficients))
+  {}
+
+  Polynomial(const std::vector<K>& coefficients) :
+    coefficients_(coefficients)
+  {}
+
+  K operator() (const K& x) const
+  {
+    auto y = K(0);
+    for (size_t i=0; i<coefficients_.size(); ++i)
+      y += coefficients_[i] * std::pow(x, i);
+    return y;
+  }
+
+  friend Polynomial derivative(const Polynomial& p)
+  {
+    auto derivative = Polynomial();
+    derivative.coefficients_.resize(p.coefficients_.size()-1);
+    for (size_t i=1; i<p.coefficients_.size(); ++i)
+      derivative.coefficients_[i-1] = p.coefficients_[i]*i;
+    return derivative;
+  }
+
+  const std::vector<K>& coefficients() const
+  {
+    return coefficients_;
+  }
+
+private:
+  std::vector<K> coefficients_;
+};
+
+
+
+}} // namespace Dune::Functions
+
+
+
+#endif // DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_POLYNOMIAL_HH
---
+++ dune/functions/analyticfunctions/trigonometricfunction.hh
@@ -0,0 +1,35 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_TRIGONOMETRICFUNCTION_HH
+#define DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_TRIGONOMETRICFUNCTION_HH
+
+
+namespace Dune {
+namespace Functions {
+
+
+
+template<class K, int sinFactor, int cosFactor>
+class TrigonometricFunction
+{
+public:
+  K operator () (const K& x) const
+  {
+    return sinFactor * std::sin(x) + cosFactor * std::cos(x);
+  }
+};
+
+
+template<class K, int sinFactor, int cosFactor>
+TrigonometricFunction<K, -cosFactor, sinFactor> derivative(const TrigonometricFunction<K, sinFactor, cosFactor>& f)
+{
+  return TrigonometricFunction<K, -cosFactor, sinFactor>();
+}
+
+
+
+}} // namespace Dune::Functions
+
+
+
+#endif // DUNE_FUNCTIONS_COMMON_ANALYTICFUNCTIONS_TRIGONOMETRICFUNCTION_HH
