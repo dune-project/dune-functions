a72826df54fb258ab8b5fb9b9efbf795a5a683f7
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-03-10 16:54:15 +0100

    [utility] Add new header type_traits
    Currently this only contains disableCopyMove because this
    requires template aliases and is thus forbidden in dune-common

---
+++ dune/functions/common/type_traits.hh
@@ -0,0 +1,22 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_COMMON_TYPE_TRAITS_HH
+#define DUNE_FUNCTIONS_COMMON_TYPE_TRAITS_HH
+
+#include <type_traits>
+
+
+namespace Dune {
+namespace Functions {
+
+
+// Helper typedef to remove constructor with forwarding reference from
+// overload set for copy and move constructor or assignment.
+template<class This, class T>
+using disableCopyMove = typename std::enable_if<not(std::is_same<This,  typename std::decay<T>::type>::value), int>::type;
+
+
+
+}} // namespace Dune::Functions
+
+#endif // DUNE_FUNCTIONS_COMMON_TYPE_TRAITS_HH
