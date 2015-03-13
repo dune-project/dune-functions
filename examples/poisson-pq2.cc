4245cfae54286edbb547fe1751338fff2263fb96
Oliver Sander <sander@igpm.rwth-aachen.de>
2015-03-13 23:01:37 +0100

    Use the new 'official' interpolation mechanism to find the Lagrange nodes
    - Allows to remove the atrocious interpolation code in the example file itself
    - Still not perfect: we hack our way around the missing support for interpolation
      of vector-valued functions.
    - Lambdas make the code so much nicer!!  A hurray for Carsten's new function interface

--- examples/poisson-pq2.cc
+++ examples/poisson-pq2.cc
@@ -16,6 +16,7 @@
 #include <dune/istl/preconditioners.hh>
 #include <dune/istl/solvers.hh>
 
+#include <dune/functions/functionspacebases/interpolate.hh>
 #include <dune/functions/functionspacebases/pq2nodalbasis.hh>
 
 using namespace Dune;
@@ -100,334 +101,6 @@ private:
   const CoefficientType &coeffs_;
   std::string s_;
 };
-
-
-/**
- *  \brief Virtual interface class for grid functions
- *
- *  A general grid function that might be defined only on parts of the underlying grid (isDefinedOn).
- *  If you have a grid function defined on a GridView derive from VirtualGridViewFunction instead.
- *
- *  \tparam GridType the underlying GridType
- *  \tparam RType the type of the range space
- *
- */
-template <class GridType, class RType>
-class VirtualGridFunction :
-  public Dune::VirtualFunction<typename GridType::template Codim<0>::Geometry::GlobalCoordinate, RType>
-{
-  protected:
-
-    typedef Dune::VirtualFunction<typename GridType::template Codim<0>::Geometry::GlobalCoordinate, RType> BaseType;
-    typedef VirtualGridFunction<GridType, RType> ThisType;
-
-  public:
-
-    typedef typename GridType::template Codim<0>::Geometry::LocalCoordinate LocalDomainType;
-    typedef typename BaseType::DomainType DomainType;
-    typedef typename BaseType::RangeType RangeType;
-
-
-    //! The grid type
-    typedef GridType Grid;
-
-    //! Element type of underlying grid
-    typedef typename Grid::template Codim<0>::Entity Element;
-
-    /** \brief Constructor
-     *
-     *  \param grid the underlying grid
-     */
-    VirtualGridFunction(const Grid& grid):
-      grid_(&grid)
-    {}
-
-    /**
-     * \brief Function evaluation in local coordinates.
-     *
-     * \param e Evaluate in local coordinates with respect to this element.
-     * \param x Argument for function evaluation in local coordinates.
-     * \param y Result of function evaluation.
-     */
-    virtual void evaluateLocal(const Element& e, const LocalDomainType& x, RangeType& y) const = 0;
-
-
-    /**
-     * \brief Check whether local evaluation is possible
-     *
-     * \param e Return true if local evaluation is possible on this element.
-     */
-    virtual bool isDefinedOn(const Element& e) const = 0;
-
-    virtual ~VirtualGridFunction() {}
-
-protected:
-    //! the underlying grid
-    const Grid* grid_;
-
-}; // end of VirtualGridFunction class
-
-
-
-
-
-/**
- * \brief Virtual interface class for grid functions associated to grid views
- *
- */
-template <class GridViewType, class RType>
-class VirtualGridViewFunction :
-  public VirtualGridFunction<typename GridViewType::Grid, RType>
-{
-  protected:
-    typedef VirtualGridFunction<typename GridViewType::Grid, RType> BaseType;
-
-  public:
-
-    typedef typename BaseType::LocalDomainType LocalDomainType;
-    typedef typename BaseType::DomainType DomainType;
-    typedef typename BaseType::RangeType RangeType;
-    typedef typename BaseType::DerivativeType DerivativeType;
-
-    typedef GridViewType GridView;
-    typedef typename BaseType::Grid Grid;
-
-    //! Element type of underlying grid
-    typedef typename Grid::template Codim<0>::Entity Element;
-
-    /**
-     *  \brief Constructor
-     *
-     *  We need to know the underlying grid view
-     */
-    VirtualGridViewFunction(const GridView& gridView) :
-      BaseType(gridView.grid()),
-      gridView_(gridView)
-    {}
-
-    /**
-     * \brief Function evaluation in local coordinates.
-     *
-     * \param e Evaluate in local coordinates with respect to this element.
-     * \param x Argument for function evaluation in local coordinates.
-     * \param y Result of function evaluation.
-     */
-    virtual void evaluateLocal(const Element& e, const LocalDomainType& x, RangeType& y) const = 0;
-
-
-    virtual ~VirtualGridViewFunction() {}
-
-    /**
-     * \brief Export underlying grid view
-     */
-    const GridView& gridView() const
-    {
-      return gridView_;
-    }
-
-
-  protected:
-
-    const GridView gridView_;
-
-}; // end of VirtualGridViewFunction class
-
-
-/** \brief Cached evaluation of local basis functions
- *
- * This class allows to evaluate single local basis functions efficiently
- * by providing a cache for the evaluation of all functions.
- *
- * \tparam Imp Implementation providing the evaluateAll method
- * \tparam FunctionBaseClass The base class for this wrapper
- */
-template <class Imp, class AllRangeType, class FunctionBaseClass>
-class CachedComponentWrapper :
-    public FunctionBaseClass
-{
-    public:
-        typedef typename FunctionBaseClass::DomainType DomainType;
-        typedef typename FunctionBaseClass::RangeType RangeType;
-
-    private:
-        typedef DomainType Position;
-        struct DomainCmp
-        {
-            bool operator() (const DomainType& a, const DomainType& b) const
-            {
-                for(int i=0; i<DomainType::dimension; ++i)
-                {
-                    if (a[i]<b[i])
-                        return true;
-                    if (a[i]>b[i])
-                        return false;
-                }
-                return false;
-            }
-        };
-        typedef typename std::map<DomainType, AllRangeType, DomainCmp> EvaluationCache;
-
-    public:
-
-        CachedComponentWrapper(const int i=0) :
-            i_(i)
-        {}
-
-        /** \brief Select local basis function to evaluate.
-         *
-         * \param i Index of the local basis function within the local basis to evaluate next.
-         */
-        void setIndex(int i)
-        {
-            i_ = i;
-        }
-
-        /** \brief Evaluate local basis function selected last using setIndex
-         *
-         * \param x Local coordinates with repect to entity given in constructor.
-         * \param y The result is stored here.
-         */
-        void evaluate(const DomainType& x, RangeType& y) const
-        {
-            typename EvaluationCache::const_iterator it = evalCache_.find(x);
-            if (it != evalCache_.end())
-                y = it->second[i_];
-            else
-            {
-                AllRangeType& yy = evalCache_[x];
-                asImp().evaluateAll(x, yy);
-                y = yy[i_];
-            }
-        }
-
-    protected:
-
-        const Imp& asImp () const
-        {
-            return static_cast<const Imp &> (*this);
-        }
-
-        int i_;
-
-        mutable EvaluationCache evalCache_;
-};
-
-
-template<class F, class Grid, class Base>
-class LocalFunctionComponentWrapper :
-    public CachedComponentWrapper<
-        LocalFunctionComponentWrapper<F, Grid, Base>,
-        typename F::RangeType,
-        Base>
-{
-    private:
-        CachedComponentWrapper<LocalFunctionComponentWrapper<F, Grid, Base>, typename F::RangeType, Base> BaseClass;
-        typedef typename Grid::template Codim<0>::Entity Element;
-        typedef typename Element::Geometry Geometry;
-    public:
-        typedef typename F::RangeType AllRangeType;
-
-        typedef typename Base::DomainType DomainType;
-        typedef typename Base::RangeType RangeType;
-
-        LocalFunctionComponentWrapper(const F& f, const Element& e, int comp) :
-            BaseClass(comp),
-            f_(f),
-            e_(e),
-            geometry_(e_.geometry())
-        {}
-
-        void evaluateAll(const DomainType& x, AllRangeType& y) const
-        {
-            typedef VirtualGridFunction<Grid, AllRangeType> GridFunction;
-
-            const GridFunction* gf = dynamic_cast<const GridFunction*>(&f_);
-            if (gf and gf->isDefinedOn(e_))
-                gf->evaluateLocal(e_, x, y);
-            else
-                f_.evaluate(geometry_.global(x), y);
-        }
-
-
-    protected:
-        const F& f_;
-        const Element& e_;
-        const Geometry geometry_;
-};
-
-
-/**
- * \brief Interpolate given function in discrete function space
- *
- * \param basis Global function space basis of discrete function space
- * \param coeff Coefficient vector to represent the interpolation
- * \param f Function to interpolate
- */
-template <class B, class C, class F>
-void interpolate(const B& basis, C& coeff, const F& f)
-{
-  typedef typename B::GridView GridView;
-  typedef typename GridView::Grid Grid;
-
-  typedef typename B::LocalView::Tree::FiniteElement LocalFiniteElement;
-
-  typedef typename Dune::LocalFiniteElementFunctionBase<LocalFiniteElement>::type FunctionBaseClass;
-  typedef LocalFunctionComponentWrapper<F, Grid, FunctionBaseClass> LocalWrapper;
-
-  const GridView& gridview = basis.gridView();
-
-  auto basisIndexSet = basis.indexSet();
-  coeff.resize(basisIndexSet.size());
-
-  typename Dune::BitSetVector<1> processed(basisIndexSet.size(), false);
-
-  std::vector<typename LocalWrapper::RangeType> interpolationValues;
-
-  typename B::LocalView localView(&basis);
-  auto localIndexSet = basisIndexSet.localIndexSet();
-
-  auto it = gridview.template begin<0>();
-  auto end = gridview.template end<0>();
-  for(; it != end; ++it)
-  {
-    localView.bind(*it);
-    localIndexSet.bind(localView);
-
-    const auto& fe = localView.tree().finiteElement();
-
-    // check if all components have already been processed
-    bool allProcessed = true;
-    for (size_t i=0; i<fe.localBasis().size(); ++i)
-      allProcessed = allProcessed and processed[localIndexSet.index(i)[0]][0];
-
-    if (not(allProcessed))
-    {
-      LocalWrapper fjLocal(f, *it, 0);
-
-      //size_t lastComponent = Components<typename F::RangeType>::size(coeff[0])-1;
-      size_t lastComponent = coeff[0].size()-1;
-      for (size_t j=0; j<=lastComponent; ++j)
-      {
-        fjLocal.setIndex(j);
-        fe.localInterpolation().interpolate(fjLocal, interpolationValues);
-
-        for (size_t i=0; i<fe.localBasis().size(); ++i)
-        {
-          size_t index = localIndexSet.index(i)[0];
-
-          // check if DOF is unprocessed
-          if (not(processed[index][0]))
-          {
-            coeff[index][j] = interpolationValues[i];
-            if (j==lastComponent)
-              processed[index][0] = true;
-          }
-        }
-      }
-    }
-  }
-}
-
 
 // THIS IS THE END OF THE BLOCK OF TEMPORARY HELPER CODE.
 // ALL CODE AFTER THIS LINE IS PART OF THE EXAMPLE PROPER.
@@ -654,18 +327,6 @@ void assembleLaplaceMatrix(const FEBasis& feBasis,
 
 }
 
-// The identity function in R^dim.
-// This function is used to find the positions of the Lagrange nodes of the FE space
-template <int dim>
-struct Identity
-    : public VirtualFunction<FieldVector<double,dim>, FieldVector<double,dim> >
-{
-    void evaluate(const FieldVector<double,dim>& in, FieldVector<double,dim>& out) const {
-        out = in;
-    }
-};
-
-
 
 // This method marks all vertices on the boundary of the grid.
 // In our problem these are precisely the Dirichlet nodes.
@@ -680,9 +341,21 @@ void boundaryTreatment (const FEBasis& feBasis,
 
   // Interpolating the identity function wrt to a Lagrange basis
   // yields the positions of the Lagrange nodes
-  Identity<dim> identity;
-  BlockVector<FieldVector<double,dim> > lagrangeNodes;
-  interpolate(feBasis, lagrangeNodes, identity);
+
+  // TODO: We are hacking our way around the fact that interpolation
+  // of vector-value functions is not supported yet.
+  BlockVector<FieldVector<double,1> > lagrangeNodes0;
+  BlockVector<FieldVector<double,1> > lagrangeNodes1;
+
+  interpolate(feBasis, lagrangeNodes0, [](FieldVector<double,dim> x){ return x[0]; });
+  interpolate(feBasis, lagrangeNodes1, [](FieldVector<double,dim> x){ return x[1]; });
+
+  BlockVector<FieldVector<double,dim> > lagrangeNodes(lagrangeNodes0.size());
+  for (size_t i=0; i<lagrangeNodes.size(); i++)
+  {
+    lagrangeNodes[i][0] = lagrangeNodes0[i];
+    lagrangeNodes[i][1] = lagrangeNodes1[i];
+  }
 
   dirichletNodes.resize(lagrangeNodes.size());
 
