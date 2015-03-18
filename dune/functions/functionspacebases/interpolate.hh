5c899fd592c5a00dd60b614da6492fe4813885bb
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-03-18 16:43:33 +0100

    Add support for interpolating a given subset of DOFs

--- dune/functions/functionspacebases/interpolate.hh
+++ dune/functions/functionspacebases/interpolate.hh
@@ -6,6 +6,7 @@
 #include <memory>
 #include <vector>
 
+#include <dune/common/exceptions.hh>
 #include <dune/common/bitsetvector.hh>
 
 #include <dune/localfunctions/common/virtualinterface.hh>
@@ -17,6 +18,21 @@ namespace Dune {
 namespace Functions {
 
 namespace Imp {
+
+struct AllTrueBitSetVector
+{
+  struct AllTrueBitSet
+  {
+      bool test(int i) const { return true; }
+  } allTrue_;
+
+  const AllTrueBitSet& operator[](int i) const
+  {
+      return allTrue_;
+  }
+};
+
+
 
 template<class C>
 struct FlatIndexContainerAccess
@@ -104,6 +120,51 @@ struct FlatIndexContainerAccess<typename std::vector<K> >
   }
 };
 
+template<int k, class Alloc>
+struct FlatIndexContainerAccess<Dune::BitSetVectorConstReference<k, Alloc> >
+{
+  typedef typename Dune::BitSetVectorConstReference<k, Alloc> RT;
+
+  template<class RFT>
+  static void setEntry(RT& x, int i, const RFT& xi)
+  {
+    DUNE_THROW(Dune::Exception, "Can't modify Dune::BitSetVector<k>::const_reference");
+  }
+
+  static bool getEntry(const RT& x, int i)
+  {
+    return x.test(i);
+  }
+
+  static int size(const RT& x)
+  {
+      return x.size();
+  }
+};
+
+template<>
+struct FlatIndexContainerAccess<AllTrueBitSetVector::AllTrueBitSet>
+{
+  template<class RFT>
+  static void setEntry(AllTrueBitSetVector::AllTrueBitSet& x, int i, const RFT& xi)
+  {
+    DUNE_THROW(Dune::Exception, "Can't modify AllTrueBitSet");
+  }
+
+  static bool getEntry(const AllTrueBitSetVector::AllTrueBitSet& x, int i)
+  {
+    return true;
+  }
+
+  static int size(const AllTrueBitSetVector::AllTrueBitSet& x)
+  {
+    DUNE_THROW(Dune::NotImplemented, "AllTrueBitSet has no size");
+  }
+};
+
+
+
+
 } // namespace Imp
 
 
@@ -119,9 +180,10 @@ struct FlatIndexContainerAccess<typename std::vector<K> >
  * \param basis Global function space basis of discrete function space
  * \param coeff Coefficient vector to represent the interpolation
  * \param f Function to interpolate
+ * \param bitVector A vector with flags marking ald DOFs that should be interpolated
  */
-template <class B, class C, class F>
-void interpolate(const B& basis, C& coeff, F&& f)
+template <class B, class C, class F, class BV>
+void interpolate(const B& basis, C& coeff, F&& f, BV&& bitVector)
 {
   using GridView = typename B::GridView;
   using Element = typename GridView::template Codim<0>::Entity;
@@ -133,6 +195,7 @@ void interpolate(const B& basis, C& coeff, F&& f)
   using LocalDomain = typename Element::Geometry::LocalCoordinate;
 
   using CoefficientBlock = typename std::decay<decltype(coeff[0])>::type;
+  using BitVectorBlock = typename std::decay<decltype(bitVector[0])>::type;
 
   auto&& gridView = basis.gridView();
 
@@ -163,6 +226,8 @@ void interpolate(const B& basis, C& coeff, F&& f)
   auto localView = basis.localView();
   auto localIndexSet = basisIndexSet.localIndexSet();
 
+  auto blockSize = Imp::FlatIndexContainerAccess<CoefficientBlock>::size(coeff[0]);
+
   for (const auto& e : elements(gridView))
   {
     localView.bind(e);
@@ -174,22 +239,39 @@ void interpolate(const B& basis, C& coeff, F&& f)
     // check if all components have already been processed
     bool allProcessed = true;
     for (size_t i=0; i<fe.localBasis().size(); ++i)
-      allProcessed = allProcessed and processed[localIndexSet.index(i)[0]][0];
+    {
+      // if index was already processed we don't need to do further checks
+      auto index = localIndexSet.index(i)[0];
+      if (processed[index][0])
+        continue;
+
+      // if index was not processed, check if any entry is marked for interpolation
+      auto&& bitVectorBlock = bitVector[index];
+      for(int k=0; k<blockSize; ++k)
+      {
+        if (Imp::FlatIndexContainerAccess<BitVectorBlock>::getEntry(bitVectorBlock,k))
+        {
+          allProcessed = false;
+          break;
+        }
+      }
+    }
 
     if (not(allProcessed))
     {
-      auto blockSize = Imp::FlatIndexContainerAccess<CoefficientBlock>::size(coeff[0]);
-
       // We loop over j defined above and thus over the components of the
       // range type of localF.
       for(j=0; j<blockSize; ++j)
       {
+
         // We cannot use localFj directly because interpolate requires a Dune::VirtualFunction like interface
         fe.localInterpolation().interpolate(FunctionFromCallable(localFj), interpolationValues);
         for (size_t i=0; i<fe.localBasis().size(); ++i)
         {
           size_t index = localIndexSet.index(i)[0];
-          if (not(processed[index][0]))
+          auto interpolateHere = Imp::FlatIndexContainerAccess<BitVectorBlock>::getEntry(bitVector[index],j);
+
+          if (not(processed[index][0]) and interpolateHere)
             Imp::FlatIndexContainerAccess<CoefficientBlock>::setEntry(coeff[index], j, interpolationValues[i]);
         }
       }
@@ -199,6 +281,24 @@ void interpolate(const B& basis, C& coeff, F&& f)
   }
 }
 
+
+
+/**
+ * \brief Interpolate given function in discrete function space
+ *
+ * Notice that this will only work if the range type of f and
+ * the block type of coeff are compatible and supported by
+ * FlatIndexContainerAccess.
+ *
+ * \param basis Global function space basis of discrete function space
+ * \param coeff Coefficient vector to represent the interpolation
+ * \param f Function to interpolate
+ */
+template <class B, class C, class F>
+void interpolate(const B& basis, C& coeff, F&& f)
+{
+  interpolate (basis, coeff, f, Imp::AllTrueBitSetVector());
+}
 
 
 } // namespace Functions
