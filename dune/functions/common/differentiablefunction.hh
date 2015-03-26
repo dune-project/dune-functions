405e81d5245f132bb7b79a64bb49f2916319b384
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-03-26 12:37:22 +0100

    Move concept definitions to seperate header

--- dune/functions/common/differentiablefunction.hh
+++ dune/functions/common/differentiablefunction.hh
@@ -8,73 +8,11 @@
 #include <dune/functions/common/type_traits.hh>
 #include <dune/functions/common/defaultderivativetraits.hh>
 #include <dune/functions/common/differentiablefunction_imp.hh>
-#include <dune/functions/common/concept.hh>
 #include <dune/functions/common/signature.hh>
 #include <dune/functions/common/typeerasure.hh>
 
 namespace Dune {
 namespace Functions {
-
-
-namespace Concept {
-
-
-
-// Function concept ############################################################
-template<class Signature>
-struct Function;
-
-template<class Range, class Domain>
-struct Function<Range(Domain)>
-{
-  template<class F>
-  auto require(F&& f) -> decltype(
-    Range(f(std::declval<Domain>()))
-  );
-};
-
-/// Check if F models the Function concept with given signature
-template<class F, class... T>
-static constexpr bool isFunction()
-{ return Concept::models<Concept::Function<T...>, F>(); }
-
-/// Check if f models the Function concept with given signature
-template<class F, class Signature, template<class> class DerivativeTraits>
-static constexpr bool isFunction(F&& f, SignatureTag<Signature, DerivativeTraits>)
-{ return Concept::models<Concept::Function<Signature>, F>(); }
-
-
-
-// DifferentiableFunction concept ##############################################
-template<class Signature, template<class> class DerivativeTraits = DefaultDerivativeTraits>
-struct DifferentiableFunction;
-
-template<class Range, class Domain, template<class> class DerivativeTraits>
-struct DifferentiableFunction<Range(Domain), DerivativeTraits>
-{
-  using DerivativeSignature = typename SignatureTraits<Range(Domain)>::template DerivativeSignature<DerivativeTraits>;
-
-  template<class F>
-  auto require(F&& f) -> decltype(
-    Range(f(std::declval<Domain>())),
-    requireTrue<isFunction<decltype(derivative(f)), DerivativeSignature>()>()
-  );
-};
-
-/// Check if F models the DifferentiableFunction concept with given signature
-template<class F, class... T>
-static constexpr bool isDifferentiableFunction()
-{ return Concept::models<Concept::DifferentiableFunction<T...>, F>(); }
-
-/// Check if f models the DifferentiableFunction concept with given signature
-template<class F, class Signature, template<class> class DerivativeTraits>
-static constexpr bool isDifferentiableFunction(F&& f, SignatureTag<Signature, DerivativeTraits>)
-{ return Concept::models<Concept::DifferentiableFunction<Signature, DerivativeTraits>, F>(); }
-
-
-
-} // namespace Dune::Functions::Concept
-
 
 
 
---
+++ dune/functions/common/functionconcepts.hh
@@ -0,0 +1,71 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_COMMON_FUNCTIONCONCEPT_HH
+#define DUNE_FUNCTIONS_COMMON_FUNCTIONCONCEPT_HH
+
+
+#include <dune/functions/common/concept.hh>
+#include <dune/functions/common/signature.hh>
+
+namespace Dune {
+namespace Functions {
+namespace Concept {
+
+
+
+// Function concept ############################################################
+template<class Signature>
+struct Function;
+
+template<class Range, class Domain>
+struct Function<Range(Domain)>
+{
+  template<class F>
+  auto require(F&& f) -> decltype(
+    Range(f(std::declval<Domain>()))
+  );
+};
+
+/// Check if F models the Function concept with given signature
+template<class F, class... T>
+static constexpr bool isFunction()
+{ return Concept::models<Concept::Function<T...>, F>(); }
+
+/// Check if f models the Function concept with given signature
+template<class F, class Signature, template<class> class DerivativeTraits>
+static constexpr bool isFunction(F&& f, SignatureTag<Signature, DerivativeTraits>)
+{ return Concept::models<Concept::Function<Signature>, F>(); }
+
+
+
+// DifferentiableFunction concept ##############################################
+template<class Signature, template<class> class DerivativeTraits = DefaultDerivativeTraits>
+struct DifferentiableFunction;
+
+template<class Range, class Domain, template<class> class DerivativeTraits>
+struct DifferentiableFunction<Range(Domain), DerivativeTraits>
+{
+  using DerivativeSignature = typename SignatureTraits<Range(Domain)>::template DerivativeSignature<DerivativeTraits>;
+
+  template<class F>
+  auto require(F&& f) -> decltype(
+    Range(f(std::declval<Domain>())),
+    requireTrue<isFunction<decltype(derivative(f)), DerivativeSignature>()>()
+  );
+};
+
+/// Check if F models the DifferentiableFunction concept with given signature
+template<class F, class... T>
+static constexpr bool isDifferentiableFunction()
+{ return Concept::models<Concept::DifferentiableFunction<T...>, F>(); }
+
+/// Check if f models the DifferentiableFunction concept with given signature
+template<class F, class Signature, template<class> class DerivativeTraits>
+static constexpr bool isDifferentiableFunction(F&& f, SignatureTag<Signature, DerivativeTraits>)
+{ return Concept::models<Concept::DifferentiableFunction<Signature, DerivativeTraits>, F>(); }
+
+
+
+}}} // namespace Dune::Functions::Concept
+
+#endif // DUNE_FUNCTIONS_COMMON_FUNCTIONCONCEPT_HH
