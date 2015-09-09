60f9c44735273158baccb8fee48df00ecdaf1dc2
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-09-09 11:20:13 +0200

    [cleanup] Properly organize in namespaces and reuse Dune::Std::integer_sequence
    Unfortunately we can't use
    
      Dune::Std::index_sequence
    
    from dune-common, because it's only partly correct due to
    missing template aliases. Instead we define our own
    
      Dune::Functions::Std::index_sequence

--- dune/functions/functionspacebases/tupletreepath.hh
+++ dune/functions/functionspacebases/tupletreepath.hh
@@ -5,71 +5,82 @@
 
 
 #include <tuple>
+#include <dune/common/std/utility.hh>
 
 
 namespace Dune {
 namespace Functions {
+
+namespace StaticIndices {
+  static const std::integral_constant<int, 0> _0;
+  static const std::integral_constant<int, 1> _1;
+  static const std::integral_constant<int, 2> _2;
+  static const std::integral_constant<int, 3> _3;
+  static const std::integral_constant<int, 4> _4;
+  static const std::integral_constant<int, 5> _5;
+  static const std::integral_constant<int, 6> _6;
+  static const std::integral_constant<int, 7> _7;
+  static const std::integral_constant<int, 8> _8;
+  static const std::integral_constant<int, 9> _9;
+}
 
 
 //
 // Some helper functionality for tuples
 namespace Std {
 
-  // A static sequence of integers
-  // just like std::integer_sequence in c++14
-  template<class T, T... i>
-  struct integer_sequence
-  {
-    using value_type = T;
-
-    static constexpr std::size_t size() noexcept
-    {
-      return sizeof...(i);
-    }
-  };
-
+  // A static sequence of indices
+  // just like std::index_sequence in c++14
+  //
+  // While we already have Dune::Std::index_sequence<...>, there's a
+  // major problem with it. The latter does only derive form
+  // Dune::Std::integer_sequence<std::size_t,...> but is not
+  // equal to. Hence all template specializations for integer_sequence
+  // (as done e.g. in the utilities below) would not work without
+  // special treatment for index_sequence. The proper implementation
+  // is given by the following alias in Dune::Functions::Std.
   template<std::size_t... i>
-  using index_sequence = integer_sequence<std::size_t, i...>;
+  using index_sequence = Dune::Std::integer_sequence<std::size_t, i...>;
+
+}
 
 
-} // namespace Std
-
-namespace Imp {
-
+namespace Imp
+{
 
   // Helper struct for construction of an
   // interger_sequence containing a range
-  template<class T, T begin, T end, class Head=Std::integer_sequence<T> >
+  template<class T, T begin, T end, class Head=Dune::Std::integer_sequence<T> >
   struct MakeIntegerRangeHelper
   {};
 
   template<class T, T begin, T end, T... I>
-  struct MakeIntegerRangeHelper<T, begin, end, Std::integer_sequence<T,I...> >
+  struct MakeIntegerRangeHelper<T, begin, end, Dune::Std::integer_sequence<T,I...> >
   {
-    using type = typename MakeIntegerRangeHelper<T, begin+1, end, Std::integer_sequence<T, I..., begin>>::type;
+    using type = typename MakeIntegerRangeHelper<T, begin+1, end, Dune::Std::integer_sequence<T, I..., begin>>::type;
   };
 
   template<class T, T end, T... I>
-  struct MakeIntegerRangeHelper<T, end, end, Std::integer_sequence<T, I...> >
+  struct MakeIntegerRangeHelper<T, end, end, Dune::Std::integer_sequence<T, I...> >
   {
-    using type = Std::integer_sequence<T, I...>;
+    using type = Dune::Std::integer_sequence<T, I...>;
   };
 
-
-  // Construct interger_sequence containg the entries begin...(end-1)
-  template<class T, T begin, T end>
-  using MakeIntegerRange = typename MakeIntegerRangeHelper<T, begin, end>::type;
-
-  // Construct index_sequence containg the entries begin...(end-1)
-  template<std::size_t begin, std::size_t end>
-  using MakeIndexRange = MakeIntegerRange<std::size_t, begin, end>;
 }
+
+// Construct interger_sequence containg the entries begin...(end-1)
+template<class T, T begin, T end>
+using MakeIntegerRange = typename Imp::MakeIntegerRangeHelper<T, begin, end>::type;
+
+// Construct index_sequence containg the entries begin...(end-1)
+template<std::size_t begin, std::size_t end>
+using MakeIndexRange = MakeIntegerRange<std::size_t, begin, end>;
 
 
 
 // build sub-tuple containing entries given indices in an index_sequence
 template<class... T, std::size_t... i>
-auto subTuple(const std::tuple<T...>& t, Std::index_sequence<i...>)
+auto subTuple(const std::tuple<T...>& t, Functions::Std::index_sequence<i...>)
   -> decltype(std::make_tuple(std::get<i>(t)...))
 {
   return std::make_tuple(std::get<i>(t)...);
@@ -79,9 +90,9 @@ auto subTuple(const std::tuple<T...>& t, Std::index_sequence<i...>)
 // of the begin-th to (end-1)-th entries of tuple
 template<std::size_t begin, std::size_t end, class... T>
 auto tupleSlice(const std::tuple<T...>& t)
-  -> decltype(subTuple(t, Imp::MakeIndexRange<begin, end>()))
+  -> decltype(subTuple(t, MakeIndexRange<begin, end>()))
 {
-  return subTuple(t, Imp::MakeIndexRange<begin, end>());
+  return subTuple(t, MakeIndexRange<begin, end>());
 }
 
 // build sub-tuple containing a the slice consisting
@@ -106,6 +117,25 @@ auto tupleTail(const std::tuple<T...>& t)
 
 
 
+int canonicalizeTreePathIndex(int i)
+{
+  return i;
+}
+
+template<int i>
+std::integral_constant<int,i> canonicalizeTreePathIndex(const std::integral_constant<int, i>& ci)
+{
+  return std::integral_constant<int,i>();
+}
+
+// Create a TreePath from arguments
+template<class... I>
+auto makeTreePath(I&&... i)
+  -> decltype(std::make_tuple(canonicalizeTreePathIndex(i)...))
+{
+  return std::make_tuple(canonicalizeTreePathIndex(i)...);
+}
+
 // Extend TreePath by static index given by integer i
 template<int i, class... H>
 std::tuple<H..., std::integral_constant<int, i>> extendTreePath(const std::tuple<H...>&head)
@@ -122,30 +152,48 @@ std::tuple<H..., int> extendTreePath(const std::tuple<H...>&head, int i)
 
 
 
-
-
-// Get child of given tree specified by given TreePath
+// Get child with empty TreePath, i.e., the tree itself
 template<class Tree>
-Tree&& getChild(Tree&& tree, const std::tuple<>& tp)
+auto getChild(Tree&& tree)
+  ->decltype(std::forward<Tree>(tree))
 {
-  return tree;
+  return std::forward<Tree>(tree);
 }
 
 // Get child of given tree specified by given TreePath
 template<class Tree, class... I>
-auto getChild(Tree&& tree, const std::tuple<int, I...>& tp)
-  -> decltype(getChild(tree.child(std::get<0>(tp)), tupleTail<1>(tp)))
+auto getChild(Tree&& tree, const int& i0, const I&... i)
+  -> decltype(getChild(tree.child(i0), i...))
 {
-  return getChild(tree.child(std::get<0>(tp)), tupleTail<1>(tp));
+  return getChild(tree.child(i0), i...);
+}
+
+template<class Tree, int i0, class... I>
+auto getChild(Tree&& tree, const std::integral_constant<int,i0>&, const I&... i)
+  -> decltype(getChild(tree. template child<i0>(), i...))
+{
+  return getChild(tree. template child<i0>(), i...);
+}
+
+namespace Imp {
+
+  template<class Tree, class... T, std::size_t... i>
+  auto getChildHelper(Tree&& tree, const std::tuple<T...>& tp, const Functions::Std::index_sequence<i...>&)
+    -> decltype(getChild(tree, std::get<i>(tp)...))
+  {
+    return getChild(tree, std::get<i>(tp)...);
+  }
+
 }
 
 // Get child of given tree specified by given TreePath
-template<class Tree, int i0, class... I>
-auto getChild(Tree&& tree, const std::tuple<std::integral_constant<int,i0>, I...>& tp)
-  -> decltype(getChild(tree.template child<i0>(), tupleTail<1>(tp)))
+template<class Tree, class... I>
+auto getChild(Tree&& tree, const std::tuple<I...>& tp)
+  -> decltype(Imp::getChildHelper(tree, tp, MakeIndexRange<0,sizeof...(I)>()))
 {
-  return getChild(tree.template child<i0>(), tupleTail<1>(tp));
+  return Imp::getChildHelper(tree, tp, MakeIndexRange<0,sizeof...(I)>());
 }
+
 
 
 } // namespace Functions
