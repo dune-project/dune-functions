c294b39794883c027411e91b7afa0340016b506a
Oliver Sander <sander@igpm.rwth-aachen.de>
2015-09-10 19:49:31 +0200

    Update PQ1NodalBasis to the new-style construction
    Following the example of PQkNodalBasis.

--- dune/functions/functionspacebases/pq1nodalbasis.hh
+++ dune/functions/functionspacebases/pq1nodalbasis.hh
@@ -11,319 +11,138 @@
 
 #include <dune/typetree/leafnode.hh>
 
-#include <dune/functions/functionspacebases/gridviewfunctionspacebasis.hh>
+#include <dune/functions/functionspacebases/nodes.hh>
+#include <dune/functions/functionspacebases/tupletreepath.hh>
+#include <dune/functions/functionspacebases/defaultglobalbasis.hh>
 
 
 namespace Dune {
 namespace Functions {
 
-template<typename GV>
-class PQ1NodalBasisLocalView;
+// *****************************************************************************
+// This is the reusable part of the basis. It contains
+//
+//   PQ1NodeFactory
+//   PQ1NodeIndexSet
+//   PQ1Node
+//
+// The factory allows to create the others and is the owner of possible shared
+// state. These three components do _not_ depend on the global basis or index
+// set and can be used without a global basis.
+// *****************************************************************************
 
-template<typename GV>
-class PQ1NodalBasisLeafNode;
+template<typename GV, typename ST, typename TP>
+class PQ1Node;
 
-template<typename GV>
-class PQ1IndexSet;
+template<typename GV, class MI, class TP, class ST>
+class PQ1NodeIndexSet;
 
-template<typename GV>
-class PQ1LocalIndexSet
-{
-  static const int dim = GV::dimension;
+template<typename GV, class MI, class ST>
+class PQ1NodeFactory;
 
-public:
-  typedef std::size_t size_type;
-
-  /** \brief Type of the local view on the restriction of the basis to a single element */
-  typedef PQ1NodalBasisLocalView<GV> LocalView;
-
-  /** \brief Type used for global numbering of the basis vectors */
-  typedef std::array<size_type, 1> MultiIndex;
-
-  PQ1LocalIndexSet()
-  {}
-
-  /** \brief Bind the view to a grid element
-   *
-   * Having to bind the view to an element before being able to actually access any of its data members
-   * offers to centralize some expensive setup code in the 'bind' method, which can save a lot of run-time.
-   */
-  void bind(const PQ1NodalBasisLocalView<GV>& localView)
-  {
-    localView_ = &localView;
-  }
-
-  /** \brief Unbind the view
-   */
-  void unbind()
-  {
-    localView_ = nullptr;
-  }
-
-  /** \brief Size of subtree rooted in this node (element-local)
-   */
-  size_type size() const
-  {
-    return localView_->tree().finiteElement_->size();
-  }
-
-  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis (pair of multi-indices)
-  const MultiIndex index(size_type i) const
-  {
-    return {{ localView_->globalBasis_->gridView().indexSet().subIndex(
-        *(localView_->element_),
-        localView_->tree().finiteElement_->localCoefficients().localKey(i).subEntity(),
-        dim) }};
-  }
-
-  /** \brief Return the local view that we are attached to
-   */
-  const LocalView& localView() const
-  {
-    return *localView_;
-  }
-
-  const PQ1NodalBasisLocalView<GV>* localView_;
-};
-
-template<typename GV>
-class PQ1IndexSet
-{
-  static const int dim = GV::dimension;
-
-public:
-
-  typedef PQ1LocalIndexSet<GV> LocalIndexSet;
-
-  PQ1IndexSet(const GV& gridView)
-    : size_(gridView.size(dim))
-  {}
-
-  std::size_t size() const
-  {
-    return size_;
-  }
-
-  LocalIndexSet localIndexSet() const
-  {
-    return LocalIndexSet();
-  }
-
-private:
-  std::size_t size_;
-};
-
-
-
-/** \brief Nodal basis of a scalar first-order Lagrangean finite element space
- *
- * \tparam GV The GridView that the space is defined on.
- */
-template<typename GV>
-class PQ1NodalBasis
-: public GridViewFunctionSpaceBasis<GV,
-                                    PQ1NodalBasisLocalView<GV>,
-                                    PQ1IndexSet<GV>,
-                                    std::array<std::size_t, 1> >
+template<typename GV, class MI, class ST>
+class PQ1NodeFactory
 {
   static const int dim = GV::dimension;
 
 public:
 
   /** \brief The grid view that the FE space is defined on */
-  typedef GV GridView;
-  typedef std::size_t size_type;
+  using GridView = GV;
+  using size_type = ST;
 
-  /** \brief Type of the local view on the restriction of the basis to a single element */
-  typedef PQ1NodalBasisLocalView<GV> LocalView;
+  template<class TP>
+  using Node = PQ1Node<GV, size_type, TP>;
+
+  template<class TP>
+  using IndexSet = PQ1NodeIndexSet<GV, MI, TP, ST>;
 
   /** \brief Type used for global numbering of the basis vectors */
-  typedef std::array<size_type, 1> MultiIndex;
+  using MultiIndex = MI;
+
+  using SizePrefix = Dune::ReservedVector<size_type, 2>;
 
   /** \brief Constructor for a given grid view object */
-  PQ1NodalBasis(const GridView& gv) :
+  PQ1NodeFactory(const GridView& gv) :
     gridView_(gv)
+  {}
+
+  void initializeIndices()
   {}
 
   /** \brief Obtain the grid view that the basis is defined on
    */
-  const GridView& gridView() const DUNE_FINAL
+  const GridView& gridView() const
   {
     return gridView_;
   }
 
-  PQ1IndexSet<GV> indexSet() const
+  template<class TP>
+  Node<TP> node(const TP& tp) const
   {
-    return PQ1IndexSet<GV>(gridView_);
+    return Node<TP>{tp};
   }
 
-  /**
-   * \brief Return local view for basis
-   *
-   */
-  LocalView localView() const
+  template<class TP>
+  IndexSet<TP> indexSet() const
   {
-    return LocalView(this);
+    return IndexSet<TP>{*this};
   }
 
-protected:
+  size_type size() const
+  {
+    return gridView_.size(dim);
+  }
+
+  //! Return number possible values for next position in multi index
+  size_type size(const SizePrefix prefix) const
+  {
+    if (prefix.size() == 0)
+      return size();
+    DUNE_THROW(RangeError, "Method size() can only be called for prefixes of length zero");
+  }
+
+  /** \todo This method has been added to the interface without prior discussion. */
+  size_type dimension() const
+  {
+    return size();
+  }
+
+  size_type maxNodeSize() const
+  {
+    return StaticPower<2,GV::dimension>::power;
+  }
+
+//protected:
   const GridView gridView_;
 };
 
 
-template<typename GV>
-class PQ1NodalBasisLocalView
-{
-  static const int dim = GV::dimension;
 
-  // allow acces to the global function space
-  friend PQ1LocalIndexSet<GV>;
-
-public:
-
-  typedef PQ1NodalBasis<GV> GlobalBasis;
-  typedef typename GlobalBasis::GridView GridView;
-
-  /** \brief The type used for sizes */
-  typedef typename GlobalBasis::size_type size_type;
-
-  /** \brief Type used to number the degrees of freedom
-   *
-   * In the case of mixed finite elements this really can be a multi-index, but for a standard
-   * P2 space this is only a single-digit multi-index, i.e., it is an integer.
-   */
-  typedef typename GlobalBasis::MultiIndex MultiIndex;
-
-  /** \brief Type of the grid element we are bound to */
-  typedef typename GridView::template Codim<0>::Entity Element;
-
-  /** \brief Tree of local finite elements / local shape function sets
-   *
-   * In the case of a P1 space this tree consists of a single leaf only,
-   * i.e., Tree is basically the type of the LocalFiniteElement
-   */
-  typedef PQ1NodalBasisLeafNode<GV> Tree;
-
-  /** \brief Construct local view for a given global finite element basis */
-  PQ1NodalBasisLocalView(const GlobalBasis* globalBasis) :
-    globalBasis_(globalBasis)
-  {}
-
-  /** \brief Bind the view to a grid element
-   *
-   * Having to bind the view to an element before being able to actually access any of its data members
-   * offers to centralize some expensive setup code in the 'bind' method, which can save a lot of run-time.
-   */
-  void bind(const Element& e)
-  {
-    element_ = &e;
-    tree_.bind(e);
-  }
-
-  /** \brief Return the grid element that the view is bound to
-   *
-   * \throws Dune::Exception if the view is not bound to anything
-   */
-  const Element& element() const
-  {
-    if (element_)
-      return *element_;
-    else
-      DUNE_THROW(Dune::Exception, "Can't query element of unbound local view");
-  }
-
-  /** \brief Unbind from the current element
-   *
-   * Calling this method should only be a hint that the view can be unbound.
-   * And indeed, in the PQ2NodalBasisView implementation this method does nothing.
-   */
-  void unbind()
-  {}
-
-  /**
-   * \brief Return the local ansatz tree associated to the bound entity
-   *
-   * \returns Tree // This is tree
-   */
-  const Tree& tree() const
-  {
-    return tree_;
-  }
-
-  /**
-   * \brief Return the local ansatz tree associated to the bound entity
-   *
-   * \returns Tree // This is tree
-   */
-  Tree& tree()
-  {
-    return tree_;
-  }
-
-  /** \brief Number of degrees of freedom on this element
-   */
-  size_type size() const
-  {
-    // We have subTreeSize==lfe.size() because we're in a leaf node.
-    return tree_.finiteElement_->size();
-  }
-
-  /**
-   * \brief Maximum local size for any element on the GridView
-   *
-   * This is the maximal size needed for local matrices
-   * and local vectors, i.e., the result is
-   *
-   * The method returns 3^dim, which is the number of degrees of freedom you get for cubes.
-   */
-  size_type maxSize() const
-  {
-    return 1<<dim;
-  }
-
-  /** \brief Return the global basis that we are a view on
-   */
-  const GlobalBasis& globalBasis() const
-  {
-    return *globalBasis_;
-  }
-
-protected:
-  const GlobalBasis* globalBasis_;
-  const Element* element_;
-  Tree tree_;
-};
-
-
-template<typename GV>
-class PQ1NodalBasisLeafNode :
+template<typename GV, typename ST, typename TP>
+class PQ1Node :
   public GridFunctionSpaceBasisLeafNodeInterface<
     typename GV::template Codim<0>::Entity,
     typename Dune::PQkLocalFiniteElementCache<typename GV::ctype, double, GV::dimension, 1>::FiniteElementType,
-    typename PQ1NodalBasis<GV>::size_type>
+    ST,
+    TP>
 {
-  typedef PQ1NodalBasis<GV> GlobalBasis;
   static const int dim = GV::dimension;
-  static const int maxSize = 1<<dim;
+  static const int maxSize = StaticPower<2,GV::dimension>::power;
 
   typedef typename GV::template Codim<0>::Entity E;
   typedef typename Dune::PQkLocalFiniteElementCache<typename GV::ctype, double, dim, 1> FiniteElementCache;
   typedef typename FiniteElementCache::FiniteElementType FE;
-  typedef typename GlobalBasis::size_type ST;
-  typedef typename GlobalBasis::MultiIndex MI;
-
-  typedef typename GlobalBasis::LocalView LocalView;
-
-  friend LocalView;
-  friend class PQ1LocalIndexSet<GV>;
 
 public:
-  typedef GridFunctionSpaceBasisLeafNodeInterface<E,FE,ST> Interface;
+  typedef GridFunctionSpaceBasisLeafNodeInterface<E,FE,ST,TP> Interface;
   typedef typename Interface::size_type size_type;
   typedef typename Interface::Element Element;
   typedef typename Interface::FiniteElement FiniteElement;
+  typedef typename Interface::TreePath TreePath;
 
-  PQ1NodalBasisLeafNode() :
+  PQ1Node(const TreePath& treePath) :
+    Interface(treePath),
     finiteElement_(nullptr),
     element_(nullptr)
   {}
@@ -343,43 +162,102 @@ public:
     return *finiteElement_;
   }
 
-  //! maximum size of subtree rooted in this node for any element of the global basis
+  /** \brief Size of subtree rooted in this node (element-local)
+   */
   size_type size() const DUNE_FINAL
   {
     // We have subTreeSize==lfe.size() because we're in a leaf node.
-    return finiteElement_->size();
+    return size_;
   }
-
-  //! Maps from subtree index set [0..subTreeSize-1] into root index set (element-local) [0..localSize-1]
-  size_type localIndex(size_type i) const DUNE_FINAL
-  {
-    return localIndices_[i];
-  }
-
-  void setLocalIndex(size_type leafindex, size_type localindex) DUNE_FINAL
-  {
-    localIndices_[leafindex] = localindex;
-  }
-
-protected:
 
   //! Bind to element.
   void bind(const Element& e)
   {
     element_ = &e;
     finiteElement_ = &(cache_.get(element_->type()));
-    for(size_type i=0; i<localIndices_.size(); ++i)
-      setLocalIndex(i, i);
+    size_ = finiteElement_->size();
   }
+
+protected:
+
+  size_type size_;
 
   FiniteElementCache cache_;
   const FiniteElement* finiteElement_;
   const Element* element_;
-  std::array<size_type, maxSize> localIndices_;
 };
+
+
+
+template<typename GV, class MI, class TP, class ST>
+class PQ1NodeIndexSet
+{
+  enum {dim = GV::dimension};
+
+public:
+
+  using size_type = ST;
+
+  /** \brief Type used for global numbering of the basis vectors */
+  using MultiIndex = MI;
+
+  using NodeFactory = PQ1NodeFactory<GV, MI, ST>;
+
+  using Node = typename NodeFactory::template Node<TP>;
+
+  PQ1NodeIndexSet(const NodeFactory& nodeFactory) :
+    nodeFactory_(&nodeFactory)
+  {}
+
+  /** \brief Bind the view to a grid element
+   *
+   * Having to bind the view to an element before being able to actually access any of its data members
+   * offers to centralize some expensive setup code in the 'bind' method, which can save a lot of run-time.
+   */
+  void bind(const Node& node)
+  {
+    node_ = &node;
+  }
+
+  /** \brief Unbind the view
+   */
+  void unbind()
+  {
+    node_ = nullptr;
+  }
+
+  /** \brief Size of subtree rooted in this node (element-local)
+   */
+  size_type size() const
+  {
+    return node_->finiteElement().size();
+  }
+
+  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
+  MultiIndex index(size_type i) const
+  {
+    Dune::LocalKey localKey = node_->finiteElement().localCoefficients().localKey(i);
+    const auto& gridIndexSet = nodeFactory_->gridView().indexSet();
+    const auto& element = node_->element();
+
+    return {{ gridIndexSet.subIndex(element,localKey.subEntity(),dim) }};
+  }
+
+protected:
+  const NodeFactory* nodeFactory_;
+
+  const Node* node_;
+};
+
+/** \brief Nodal basis of a scalar first-order Lagrangian finite element space
+ *
+ * \tparam GV The GridView that the space is defined on
+ * \tparam ST The type used for local indices; global indices are std::array<ST,1>
+ */
+template<typename GV, class ST = std::size_t>
+using PQ1NodalBasis = DefaultGlobalBasis<PQ1NodeFactory<GV, std::array<ST, 1>, ST> >;
 
 } // end namespace Functions
 } // end namespace Dune
-
 
 #endif // DUNE_FUNCTIONS_FUNCTIONSPACEBASES_PQ1NODALBASIS_HH
