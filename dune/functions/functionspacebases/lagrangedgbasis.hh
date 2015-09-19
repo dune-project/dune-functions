60f1bfb0e7c3c7cdf394bade9ccc922cab46a5b7
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-09-19 02:01:02 +0200

    Adapt LagrangeDGBasis to the new interface
    This amounts in implementing size and index methods.
    The rest is basically the same as for PQk. We can
    even reuse the PQkNode class.
    
    Now the gridviewfunctionspacebasistest does no longer fail
    because of LagrangeDG, but only because of PQ2 and BSpline.

--- dune/functions/functionspacebases/lagrangedgbasis.hh
+++ dune/functions/functionspacebases/lagrangedgbasis.hh
@@ -8,152 +8,79 @@
 #include <dune/common/version.hh>
 #include <dune/common/std/final.hh>
 
-#include <dune/localfunctions/lagrange/pqkfactory.hh>
-
-#include <dune/typetree/leafnode.hh>
-
 #include <dune/functions/functionspacebases/gridviewfunctionspacebasis.hh>
+#include <dune/functions/functionspacebases/nodes.hh>
+#include <dune/functions/functionspacebases/defaultglobalbasis.hh>
+#include <dune/functions/functionspacebases/flatmultiindex.hh>
+#include <dune/functions/functionspacebases/pqknodalbasis.hh>
+
+
 
 
 namespace Dune {
 namespace Functions {
 
-template<typename GV, int k>
-class LagrangeDGBasisLocalView;
 
-template<typename GV, int k>
-class LagrangeDGBasisLeafNode;
 
-template<typename GV, int k>
-class LagrangeDGIndexSet;
+// *****************************************************************************
+// This is the reusable part of the basis. It contains
+//
+//   LagrangeDGNodeFactory
+//   LagrangeDGNodeIndexSet
+//   LagrangeDGNode
+//
+// The factory allows to create the others and is the owner of possible shared
+// state. These three components do _not_ depend on the global basis or index
+// set and can be used without a global basis.
+// *****************************************************************************
 
-template<typename GV, int k>
-class LagrangeDGLocalIndexSet
+template<typename GV, int k, typename ST, typename TP>
+using LagrangeDGNode = PQkNode<GV, k, ST, TP>;
+
+template<typename GV, int k, class MI, class TP, class ST>
+class LagrangeDGNodeIndexSet;
+
+
+template<typename GV, int k, class MI, class ST>
+class LagrangeDGNodeFactory
 {
   static const int dim = GV::dimension;
 
 public:
-  typedef std::size_t size_type;
 
-  /** \brief Type of the local view on the restriction of the basis to a single element */
-  typedef LagrangeDGBasisLocalView<GV,k> LocalView;
+  /** \brief The grid view that the FE space is defined on */
+  using GridView = GV;
+  using size_type = ST;
 
-  /** \brief Type used for global numbering of the basis vectors */
-  typedef std::array<size_type, 1> MultiIndex;
-
-  LagrangeDGLocalIndexSet(const LagrangeDGIndexSet<GV,k> & indexSet)
-  : basisIndexSet_(indexSet)
-  {}
-
-  /** \brief Bind the view to a grid element
-   *
-   * Having to bind the view to an element before being able to actually access any of its data members
-   * offers to centralize some expensive setup code in the 'bind' method, which can save a lot of run-time.
-   */
-  void bind(const LagrangeDGBasisLocalView<GV,k>& localView)
-  {
-    localView_ = &localView;
-  }
-
-  /** \brief Unbind the view
-   */
-  void unbind()
-  {
-    localView_ = nullptr;
-  }
-
-  /** \brief Size of subtree rooted in this node (element-local)
-   */
-  size_type size() const
-  {
-    return localView_->tree().finiteElement_->size();
-  }
-
-  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
-  const MultiIndex index(size_type i) const
-  {
-    const auto& gridIndexSet = basisIndexSet_.gridView_.indexSet();
-    const auto& element = localView_->element();
-
-    switch (dim)
-    {
-      case 1:
-      {
-        return {basisIndexSet_.dofsPerEdge*gridIndexSet.subIndex(element,0,0) + i};
-      }
-      case 2:
-      {
-        if (element.type().isTriangle())
-        {
-          return {basisIndexSet_.dofsPerTriangle*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else if (element.type().isQuadrilateral())
-        {
-          return { basisIndexSet_.quadrilateralOffset_ + basisIndexSet_.dofsPerQuad*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else
-          DUNE_THROW(Dune::NotImplemented, "2d elements have to be triangles or quadrilaterals");
-      }
-      case 3:
-      {
-        if (element.type().isTetrahedron())
-        {
-          return {basisIndexSet_.dofsPerTetrahedron*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else if (element.type().isPrism())
-        {
-          return { basisIndexSet_.prismOffset_ + basisIndexSet_.dofsPerPrism*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else if (element.type().isHexahedron())
-        {
-          return { basisIndexSet_.hexahedronOffset_ + basisIndexSet_.dofsPerHexahedron*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else if (element.type().isPyramid())
-        {
-          return { basisIndexSet_.pyramidOffset_ + basisIndexSet_.dofsPerPyramid*gridIndexSet.subIndex(element,0,0) + i};
-        }
-        else
-          DUNE_THROW(Dune::NotImplemented, "3d elements have to be tetrahedrons, prisms, hexahedrons or pyramids");
-      }
-    }
-    DUNE_THROW(Dune::NotImplemented, "No index method for " << dim << "d grids available yet!");
-  }
-
-  /** \brief Return the local view that we are attached to
-   */
-  const LocalView& localView() const
-  {
-    return *localView_;
-  }
-
-  const LagrangeDGBasisLocalView<GV,k>* localView_;
-
-  const LagrangeDGIndexSet<GV,k> basisIndexSet_;
-};
-
-template<typename GV, int k>
-class LagrangeDGIndexSet
-{
-  static const int dim = GV::dimension;
-
-  // Needs the mapper
-  friend class LagrangeDGLocalIndexSet<GV,k>;
 
   // Precompute the number of dofs per entity type
-  const int dofsPerEdge        = k+1;
-  const int dofsPerTriangle    = ((k+1)*(k+2))/2;
-  const int dofsPerQuad        = (k+1)*(k+1);
-  const int dofsPerTetrahedron = (k+1)*(k+2)*(k+3)/6;
-  const int dofsPerPrism       = ((k+1)*(k+1)*(k+2))/2;
-  const int dofsPerHexahedron  = (k+1)*(k+1)*(k+1);
-  const int dofsPerPyramid     = ((k+1)*(k+2)*(2*k+3))/6;
+  const static int dofsPerEdge        = k-1;
+  const static int dofsPerTriangle    = (k-1)*(k-2)/2;
+  const static int dofsPerQuad        = (k-1)*(k-1);
+  const static int dofsPerTetrahedron = ((k-3)*(k-2)*(k-1)/6 > 0) ? (k-3)*(k-2)*(k-1)/6  : 0;
+  const static int dofsPerPrism       = (k-1)*(k-1)*(k-2)/2;
+  const static int dofsPerHexahedron  = (k-1)*(k-1)*(k-1);
+  const static int dofsPerPyramid     = ((k-2)*(k-1)*(2*k-3))/6;
 
-public:
 
-  typedef LagrangeDGLocalIndexSet<GV,k> LocalIndexSet;
+  template<class TP>
+  using Node = LagrangeDGNode<GV, k, size_type, TP>;
 
-  LagrangeDGIndexSet(const GV& gridView)
-  : gridView_(gridView)
+  template<class TP>
+  using IndexSet = LagrangeDGNodeIndexSet<GV, k, MI, TP, ST>;
+
+  /** \brief Type used for global numbering of the basis vectors */
+  using MultiIndex = MI;
+
+  using SizePrefix = Dune::ReservedVector<size_type, 2>;
+
+  /** \brief Constructor for a given grid view object */
+  LagrangeDGNodeFactory(const GridView& gv) :
+    gridView_(gv)
+  {}
+
+
+  void initializeIndices()
   {
     switch (dim)
     {
@@ -186,7 +113,26 @@ public:
     }
   }
 
-  std::size_t size() const
+  /** \brief Obtain the grid view that the basis is defined on
+   */
+  const GridView& gridView() const
+  {
+    return gridView_;
+  }
+
+  template<class TP>
+  Node<TP> node(const TP& tp) const
+  {
+    return Node<TP>{tp};
+  }
+
+  template<class TP>
+  IndexSet<TP> indexSet() const
+  {
+    return IndexSet<TP>{*this};
+  }
+
+  size_type size() const
   {
     switch (dim)
     {
@@ -209,123 +155,58 @@ public:
         return dofsPerTetrahedron*gridView_.size(tetrahedron) + dofsPerPyramid*gridView_.size(pyramid)
              + dofsPerPrism*gridView_.size(prism) + dofsPerHexahedron*gridView_.size(hexahedron);
       }
-
     }
-
     DUNE_THROW(Dune::NotImplemented, "No size method for " << dim << "d grids available yet!");
   }
 
-  LocalIndexSet localIndexSet() const
+  //! Return number possible values for next position in multi index
+  size_type size(const SizePrefix prefix) const
   {
-    return LocalIndexSet(*this);
+    if (prefix.size() == 0)
+      return size();
+    assert(false);
   }
 
-private:
+  /** \todo This method has been added to the interface without prior discussion. */
+  size_type dimension() const
+  {
+    return size();
+  }
+
+  size_type maxNodeSize() const
+  {
+    return StaticPower<(k+1),GV::dimension>::power;
+  }
+
+//protected:
+  const GridView gridView_;
 
   size_t quadrilateralOffset_;
   size_t pyramidOffset_;
   size_t prismOffset_;
   size_t hexahedronOffset_;
-
-  const GV gridView_;
 };
 
-/** \brief Lagrangian DG finite element space, of fixed polynomial order
- *
- * Grids may have more than one element type.
- *
- * \note This only works for 1d, 2d, or 3d grids
- *
- * \tparam GV The GridView that the space is defined on
- * \tparam k The order of the basis
- */
-template<typename GV, int k>
-class LagrangeDGBasis
-: public GridViewFunctionSpaceBasis<GV,
-                                    LagrangeDGBasisLocalView<GV,k>,
-                                    LagrangeDGIndexSet<GV,k>,
-                                    std::array<std::size_t, 1> >
+
+
+template<typename GV, int k, class MI, class TP, class ST>
+class LagrangeDGNodeIndexSet
 {
-  static const int dim = GV::dimension;
+  enum {dim = GV::dimension};
 
 public:
 
-  /** \brief The grid view that the FE space is defined on */
-  typedef GV GridView;
-  typedef std::size_t size_type;
-
-  /** \brief Type of the local view on the restriction of the basis to a single element */
-  typedef LagrangeDGBasisLocalView<GV,k> LocalView;
+  using size_type = ST;
 
   /** \brief Type used for global numbering of the basis vectors */
-  typedef std::array<size_type, 1> MultiIndex;
+  using MultiIndex = MI;
 
-  /** \brief Constructor for a given grid view object */
-  LagrangeDGBasis(const GridView& gv) :
-    gridView_(gv),
-    indexSet_(gv)
-  {}
+  using NodeFactory = LagrangeDGNodeFactory<GV, k, MI, ST>;
 
-  /** \brief Obtain the grid view that the basis is defined on
-   */
-  const GridView& gridView() const DUNE_FINAL
-  {
-    return gridView_;
-  }
+  using Node = typename NodeFactory::template Node<TP>;
 
-  LagrangeDGIndexSet<GV,k> indexSet() const
-  {
-    return indexSet_;
-  }
-
-  /** \brief Return local view for basis
-   *
-   */
-  LocalView localView() const
-  {
-    return LocalView(this);
-  }
-
-protected:
-  const GridView gridView_;
-
-  LagrangeDGIndexSet<GV,k> indexSet_;
-};
-
-
-/** \brief The restriction of a finite element basis to a single element */
-template<typename GV, int k>
-class LagrangeDGBasisLocalView
-{
-public:
-  /** \brief The global FE basis that this is a view on */
-  typedef LagrangeDGBasis<GV,k> GlobalBasis;
-  typedef typename GlobalBasis::GridView GridView;
-
-  /** \brief The type used for sizes */
-  typedef typename GlobalBasis::size_type size_type;
-
-  /** \brief Type used to number the degrees of freedom
-   *
-   * In the case of mixed finite elements this really can be a multi-index, but for a standard
-   * P3 space this is only a single-digit multi-index, i.e., it is an integer.
-   */
-  typedef typename GlobalBasis::MultiIndex MultiIndex;
-
-  /** \brief Type of the grid element we are bound to */
-  typedef typename GridView::template Codim<0>::Entity Element;
-
-  /** \brief Tree of local finite elements / local shape function sets
-   *
-   * In the case of a P3 space this tree consists of a single leaf only,
-   * i.e., Tree is basically the type of the LocalFiniteElement
-   */
-  typedef LagrangeDGBasisLeafNode<GV,k> Tree;
-
-  /** \brief Construct local view for a given global finite element basis */
-  LagrangeDGBasisLocalView(const GlobalBasis* globalBasis) :
-    globalBasis_(globalBasis),
-    tree_(globalBasis)
+  LagrangeDGNodeIndexSet(const NodeFactory& nodeFactory) :
+    nodeFactory_(&nodeFactory)
   {}
 
   /** \brief Bind the view to a grid element
@@ -333,157 +214,101 @@ public:
    * Having to bind the view to an element before being able to actually access any of its data members
    * offers to centralize some expensive setup code in the 'bind' method, which can save a lot of run-time.
    */
-  void bind(const Element& e)
+  void bind(const Node& node)
   {
-    element_ = &e;
-    tree_.bind(e);
+    node_ = &node;
   }
 
-  /** \brief Return the grid element that the view is bound to
-   *
-   * \throws Dune::Exception if the view is not bound to anything
-   */
-  const Element& element() const
-  {
-    if (element_)
-      return *element_;
-    else
-      DUNE_THROW(Dune::Exception, "Can't query element of unbound local view");
-  }
-
-  /** \brief Unbind from the current element
-   *
-   * Calling this method should only be a hint that the view can be unbound.
-   * And indeed, in the LagrangeDGBasisView implementation this method does nothing.
+  /** \brief Unbind the view
    */
   void unbind()
-  {}
-
-  /** \brief Return the local ansatz tree associated to the bound entity
-   *
-   * \returns Tree // This is tree
-   */
-  const Tree& tree() const
   {
-    return tree_;
-  }
-
-  /** \brief Number of degrees of freedom on this element
-   */
-  size_type size() const
-  {
-    // We have subTreeSize==lfe.size() because we're in a leaf node.
-    return tree_.finiteElement_->size();
-  }
-
-  /**
-   * \brief Maximum local size for any element on the GridView
-   *
-   * This is the maximal size needed for local matrices
-   * and local vectors, i.e., the result is
-   *
-   * The method returns (k+1)^dim, which is the number of degrees of freedom you get for cubes.
-   */
-  size_type maxSize() const
-  {
-    return StaticPower<(k+1),GV::dimension>::power;
-  }
-
-  /** \brief Return the global basis that we are a view on
-   */
-  const GlobalBasis& globalBasis() const
-  {
-    return *globalBasis_;
-  }
-
-protected:
-  const GlobalBasis* globalBasis_;
-  const Element* element_;
-  Tree tree_;
-};
-
-
-template<typename GV, int k>
-class LagrangeDGBasisLeafNode :
-  public GridFunctionSpaceBasisLeafNodeInterface<
-    typename GV::template Codim<0>::Entity,
-    typename Dune::PQkLocalFiniteElementCache<typename GV::ctype, double, GV::dimension, k>::FiniteElementType,
-    typename LagrangeDGBasis<GV,k>::size_type>
-{
-  typedef LagrangeDGBasis<GV,k> GlobalBasis;
-  static const int dim = GV::dimension;
-
-  typedef typename GV::template Codim<0>::Entity E;
-  typedef typename Dune::PQkLocalFiniteElementCache<typename GV::ctype, double, dim, k> FiniteElementCache;
-  typedef typename FiniteElementCache::FiniteElementType FE;
-  typedef typename GlobalBasis::size_type ST;
-  typedef typename GlobalBasis::MultiIndex MI;
-
-  typedef typename GlobalBasis::LocalView LocalView;
-
-  friend LocalView;
-  friend class LagrangeDGLocalIndexSet<GV,k>;
-
-public:
-  typedef GridFunctionSpaceBasisLeafNodeInterface<E,FE,ST> Interface;
-  typedef typename Interface::size_type size_type;
-  typedef typename Interface::Element Element;
-  typedef typename Interface::FiniteElement FiniteElement;
-
-  LagrangeDGBasisLeafNode(const GlobalBasis* globalBasis) :
-    globalBasis_(globalBasis),
-    finiteElement_(nullptr),
-    element_(nullptr)
-  {}
-
-  //! Return current element, throw if unbound
-  const Element& element() const DUNE_FINAL
-  {
-    return *element_;
-  }
-
-  /** \brief Return the LocalFiniteElement for the element we are bound to
-   *
-   * The LocalFiniteElement implements the corresponding interfaces of the dune-localfunctions module
-   */
-  const FiniteElement& finiteElement() const DUNE_FINAL
-  {
-    return *finiteElement_;
+    node_ = nullptr;
   }
 
   /** \brief Size of subtree rooted in this node (element-local)
    */
-  size_type size() const DUNE_FINAL
+  size_type size() const
   {
-    // We have subTreeSize==lfe.size() because we're in a leaf node.
-    return finiteElement_->size();
+    return node_->finiteElement().size();
   }
 
-  //! Maps from subtree index set [0..subTreeSize-1] into root index set (element-local) [0..localSize-1]
-  size_type localIndex(size_type i) const DUNE_FINAL
+  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
+  MultiIndex index(size_type i) const
   {
-    return i;
-  }
+    const auto& gridIndexSet = nodeFactory_->gridView().indexSet();
+    const auto& element = node_->element();
 
-  void setLocalIndex(size_type leafindex, size_type localindex) DUNE_FINAL
-  {
-    DUNE_THROW(Dune::NotImplemented, "LagrangeDGBasisLeafNode does not support setLocalIndex() yet.");
+    switch (dim)
+    {
+      case 1:
+      {
+        return {nodeFactory_->dofsPerEdge*gridIndexSet.subIndex(element,0,0) + i};
+      }
+      case 2:
+      {
+        if (element.type().isTriangle())
+        {
+          return {nodeFactory_->dofsPerTriangle*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else if (element.type().isQuadrilateral())
+        {
+          return { nodeFactory_->quadrilateralOffset_ + nodeFactory_->dofsPerQuad*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else
+          DUNE_THROW(Dune::NotImplemented, "2d elements have to be triangles or quadrilaterals");
+      }
+      case 3:
+      {
+        if (element.type().isTetrahedron())
+        {
+          return {nodeFactory_->dofsPerTetrahedron*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else if (element.type().isPrism())
+        {
+          return { nodeFactory_->prismOffset_ + nodeFactory_->dofsPerPrism*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else if (element.type().isHexahedron())
+        {
+          return { nodeFactory_->hexahedronOffset_ + nodeFactory_->dofsPerHexahedron*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else if (element.type().isPyramid())
+        {
+          return { nodeFactory_->pyramidOffset_ + nodeFactory_->dofsPerPyramid*gridIndexSet.subIndex(element,0,0) + i};
+        }
+        else
+          DUNE_THROW(Dune::NotImplemented, "3d elements have to be tetrahedrons, prisms, hexahedrons or pyramids");
+      }
+    }
+    DUNE_THROW(Dune::NotImplemented, "No index method for " << dim << "d grids available yet!");
   }
 
 protected:
+  const NodeFactory* nodeFactory_;
 
-  //! Bind to element.
-  void bind(const Element& e)
-  {
-    element_ = &e;
-    finiteElement_ = &(cache_.get(element_->type()));
-  }
-
-  const GlobalBasis* globalBasis_;
-  FiniteElementCache cache_;
-  const FiniteElement* finiteElement_;
-  const Element* element_;
+  const Node* node_;
 };
+
+
+
+// *****************************************************************************
+// This is the actual global basis implementation based on the reusable parts.
+// *****************************************************************************
+
+/** \brief Basis of a scalar k-th-order Lagrangean-DG finite element space
+ *
+ * \note This only works for certain grids.  The following restrictions hold
+ * - If k is no larger than 2, then the grids can have any dimension
+ * - If k is larger than 3 then the grid must be two-dimensional
+ * - If k is 3, then the grid can be 3d *if* it is a simplex grid
+ *
+ * \tparam GV The GridView that the space is defined on
+ * \tparam k The order of the basis
+ */
+template<typename GV, int k, class ST = std::size_t>
+using LagrangeDGBasis = DefaultGlobalBasis<LagrangeDGNodeFactory<GV, k, FlatMultiIndex<ST>, ST> >;
+
+
 
 } // end namespace Functions
 } // end namespace Dune
