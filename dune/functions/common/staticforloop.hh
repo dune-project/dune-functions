95d64b979fd94494111f2e592f68fec4901e2695
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-09-30 17:49:54 +0200

    Add StaticFindInRange
    This is exactly like a static for loop except, that
    it terminates if the functor returns true.

--- dune/functions/common/staticforloop.hh
+++ dune/functions/common/staticforloop.hh
@@ -49,6 +49,45 @@ void staticForLoop(F&& f, Args&&... args)
 
 
 
+
+template<class ST, ST begin, ST end>
+struct StaticFindInRange
+{
+  template<class F, class...Args>
+  static void apply(F&& f, Args&&... args)
+  {
+    if (f(std::integral_constant<ST, begin>(), std::forward<Args>(args)...))
+      return;
+    StaticFindInRange<ST, begin+1, end>::apply(std::forward<F>(f), std::forward<Args>(args)...);
+  }
+};
+
+template<class ST, ST end>
+struct StaticFindInRange<ST, end, end>
+{
+  template<class F, class...Args>
+  static void apply(F&& f, Args&&...)
+  {}
+};
+
+/**
+ * \brief Static find loop
+ *
+ * Run static for-loop from 'begin' to 'end-1' with functor.
+ * The functor is called with TypeTree::index_constant<i>
+ * as first argument. All other arguments of this method
+ * are forwarded to the functor. If the functor returns
+ * true the loop is terminated.
+ *
+ * \todo Should this be just the StaticForLoop?
+ */
+template<std::size_t begin_t, std::size_t end_t, class F, class... Args>
+void staticFindInRange(F&& f, Args&&... args)
+{
+  StaticFindInRange<std::size_t, begin_t, end_t>::apply(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+
 } // namespace Dune::Functions
 } // namespace Dune
 
