7abd577b354363c0e220e616c41cbcb207c5b7c5
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-09-30 17:55:41 +0200

    Add basistags.hh

---
+++ dune/functions/functionspacebases/basistags.hh
@@ -0,0 +1,25 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_FUNCTIONSPACEBASES_BASISTAGS_HH
+#define DUNE_FUNCTIONS_FUNCTIONSPACEBASES_BASISTAGS_HH
+
+
+
+namespace Dune {
+namespace Functions {
+namespace BasisTags {
+
+
+
+struct BlockBackTag {};
+struct BlockFrontTag {};
+struct FlatTag {};
+
+
+
+} // end namespace BasisTags
+} // end namespace Functions
+} // end namespace Dune
+
+
+#endif // DUNE_FUNCTIONS_FUNCTIONSPACEBASES_BASISTAGS_HH
