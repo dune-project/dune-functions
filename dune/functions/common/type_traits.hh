e81c9fe19e173a6ee5bb9c73727a9cc522b538b6
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-10-04 21:30:39 +0200

    Make disableCopyMove work with argument lists

--- dune/functions/common/type_traits.hh
+++ dune/functions/common/type_traits.hh
@@ -12,10 +12,10 @@ namespace Functions {
 
 // Helper typedef to remove constructor with forwarding reference from
 // overload set for copy and move constructor or assignment.
-template<class This, class T>
+template<class This, class... T>
 using disableCopyMove = typename std::enable_if<
-  (not(std::is_same<This,  typename std::decay<T>::type>::value)
-  and not(std::is_base_of<This,T>::value)), int>::type;
+  (not(std::is_same<This, typename std::tuple_element<0, std::tuple<typename std::decay<T>::type...> >::type >::value)
+  and not(std::is_base_of<This, typename std::tuple_element<0, std::tuple<typename std::decay<T>::type...> >::type >::value)), int>::type;
 
 
 
