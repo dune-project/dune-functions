84b75e6910b814f6b7b36755d0934165e0669bbe
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-10-07 14:00:21 +0200

    [concept] Add concepts for BasisNode and LeafBasisNode

--- dune/functions/functionspacebases/concepts.hh
+++ dune/functions/functionspacebases/concepts.hh
@@ -6,6 +6,8 @@
 
 #include <dune/functions/common/concept.hh>
 
+#include <dune/functions/functionspacebases/nodes.hh>
+
 
 namespace Dune {
 namespace Functions {
@@ -38,6 +40,40 @@ struct HasIndexAccess
   template<class C, class I>
   auto require(C&& c, I&& i) -> decltype(
     c[i]
+  );
+};
+
+
+// Concept for a BasisNode in a local ansatz tree
+struct BasisNode
+{
+  template<class N>
+  auto require(N&& node) -> decltype(
+    requireType<typename N::size_type>(),
+    requireType<typename N::TreePath>(),
+    requireConvertible<typename N::size_type>(node.size()),
+    requireConvertible<typename N::size_type>(node.offset()),
+    requireConvertible<typename N::size_type>(node.localIndex(std::declval<typename N::size_type>())),
+    requireConvertible<typename N::size_type>(node.treeIndex()),
+    requireConvertible<typename N::TreePath>(node.treePath()),
+    requireBaseOf<BasisNodeMixin<typename N::size_type, typename N::TreePath>, N>()
+  );
+};
+
+
+
+// Concept for a LeafBasisNode in a local ansatz tree
+template<class GridView>
+struct LeafBasisNode : Refines<BasisNode>
+{
+  template<class N>
+  auto require(N&& node) -> decltype(
+    requireType<typename N::Element>(),
+    requireType<typename N::FiniteElement>(),
+    requireConvertible<typename N::Element>(node.element()),
+    requireConvertible<const typename N::FiniteElement&>(node.finiteElement()),
+    requireConvertible<typename N::Element>(*(std::declval<GridView>().template begin<0>())),
+    requireBaseOf<Dune::Functions::LeafBasisNode<typename N::size_type, typename N::TreePath>, N>()
   );
 };
 
