e8e66b4cbbd99a9f5136cc1940bb824ecf526ad9
Carsten Gräser <graeser@mi.fu-berlin.de>
2015-10-08 14:58:54 +0200

    [utility] Add some more tuple utilities
    Most of this can be viewed as a cleaned up version of what's
    in dune-common's tupleutility.hh such that it plays nicely
    with modern C++ (lambdas, template aliases).

--- dune/functions/common/utility.hh
+++ dune/functions/common/utility.hh
@@ -62,15 +62,15 @@ auto forwardAsStaticIndex(const size_type& i, F&& f, Args&&... args)
 
 namespace Imp {
 
-template<template<class...> class T, class Tuple>
-struct ExpandTupleHelper
-{};
+  template<template<class...> class T, class Tuple>
+  struct ExpandTupleHelper
+  {};
 
-template<template<class...> class T, class... Args>
-struct ExpandTupleHelper<T, typename std::tuple<Args...>>
-{
-  using Type = T<Args...>;
-};
+  template<template<class...> class T, class... Args>
+  struct ExpandTupleHelper<T, typename std::tuple<Args...>>
+  {
+    using Type = T<Args...>;
+  };
 
 } // end namespace Imp
 
@@ -85,6 +85,117 @@ struct ExpandTupleHelper<T, typename std::tuple<Args...>>
  */
 template<template<class...> class T, class ArgTuple>
 using ExpandTuple = typename Imp::ExpandTupleHelper<T, ArgTuple>::Type;
+
+
+
+namespace Imp {
+
+  template<template<class...> class T, class... Tuple>
+  struct TransformTupleHelper
+  {};
+
+  template<template<class...> class T, class... Args1>
+  struct TransformTupleHelper<T, typename std::tuple<Args1...>>
+  {
+    using Type = std::tuple<T<Args1>...>;
+  };
+
+  template<template<class...> class T, class... Args1, class... Args2>
+  struct TransformTupleHelper<T, typename std::tuple<Args1...>, typename std::tuple<Args2...>>
+  {
+    using Type = std::tuple<T<Args1, Args2>...>;
+  };
+
+} // end namespace Imp
+
+/**
+ * \brief Transform tuple types argument using type-functor
+ *
+ * This is a template alias for a tuple whose i-th type
+ * is given by F<T1i,...,TMi> where T1i,...,TMi are the
+ * i-th types of the 1,...,M-th tuple of the given tuple
+ * list Tuples. Currently only M=1,2 are supported.
+ * \tparam F A template alias mapping 1,...,sizeof...(ArgTuple) types to a new one
+ * \tparam Tuples A list of tuples
+ */
+template<template<class...> class F, class... Tuples>
+using TransformTuple = typename Imp::TransformTupleHelper<F, Tuples...>::Type;
+
+
+
+namespace Imp {
+
+  template<class F, class... T, std::size_t... k>
+  auto transformTupleHelper(F&& f, const std::tuple<T...>& tuple, Dune::TypeTree::Std::index_sequence<k...>)
+    -> decltype(std::make_tuple(f(std::get<k>(tuple))...))
+  {
+    return std::make_tuple(f(std::get<k>(tuple))...);
+  };
+
+  template<class F, class... T1, class...T2, std::size_t... k>
+  auto transformTupleHelper(F&& f, const std::tuple<T1...>& tuple1, const std::tuple<T2...>& tuple2, Dune::TypeTree::Std::index_sequence<k...>)
+    -> decltype(std::make_tuple(f(std::get<k>(tuple1), std::get<k>(tuple2))...))
+  {
+    return std::make_tuple(f(std::get<k>(tuple1), std::get<k>(tuple2))...);
+  };
+
+} // end namespace Imp
+
+/**
+ * \brief Transform tuple value using a functor
+ *
+ * This will apply the given functor to all values in
+ * given tuple and return the results in a new tuple.
+ *
+ * \param F A functor defined for all tuple entries
+ * \param tuple The tuple to transform
+ */
+template<class F, class... T>
+auto transformTuple(F&& f, const std::tuple<T...>& tuple)
+  -> decltype(Imp::transformTupleHelper(std::forward<F>(f), tuple, Dune::TypeTree::Std::make_index_sequence<sizeof...(T)>()))
+{
+  return Imp::transformTupleHelper(std::forward<F>(f), tuple, Dune::TypeTree::Std::make_index_sequence<sizeof...(T)>());
+}
+
+/**
+ * \brief Transform tuple value using a binary functor
+ *
+ * This will apply the given functor to the each corresponding
+ * pair of values in the given tuples and return the results
+ * in a new tuple.
+ *
+ * \param F A functor defined for all tuple entries
+ * \param tuple1 The tuple containing values for the first parameter
+ * \param tuple2 The tuple containing values for the second parameter
+ */
+template<class F, class... T1, class... T2>
+auto transformTuple(F&& f, const std::tuple<T1...>& tuple1, const std::tuple<T2...>& tuple2)
+  -> decltype(Imp::transformTupleHelper(std::forward<F>(f), tuple1, tuple2, Dune::TypeTree::Std::make_index_sequence<sizeof...(T1)>()))
+{
+  return Imp::transformTupleHelper(std::forward<F>(f), tuple1, tuple2, Dune::TypeTree::Std::make_index_sequence<sizeof...(T1)>());
+}
+
+
+
+namespace Imp {
+
+  template<class IntegerSequence>
+  struct IntegerSequenceTupleHelper
+  {};
+
+  template<class I, I... k>
+  struct IntegerSequenceTupleHelper<Dune::TypeTree::Std::integer_sequence<I, k...>>
+  {
+    using Type = std::tuple<std::integral_constant<I, k>...>;
+  };
+
+} // end namespace Imp
+
+/**
+ * \brief Transform integer_sequence<I,k...> to tuple<integral_constant<I,k>...>
+ */
+template<class IntegerSequence>
+using IntegerSequenceTuple= typename Imp::IntegerSequenceTupleHelper<IntegerSequence>::Type;
 
 
 
