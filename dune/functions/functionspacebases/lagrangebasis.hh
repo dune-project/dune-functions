df46e2f2e4e21ad1dbf33934fee8434d76607237
Carsten Gräser <graeser@mi.fu-berlin.de>
2016-04-01 16:55:07 +0200

    Add LagrangeBasis
    This is just an alias for the former PQkNodalBasis which
    will perhaps be removed

---
+++ dune/functions/functionspacebases/lagrangebasis.hh
@@ -0,0 +1,49 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_FUNCTIONSPACEBASES_LAGRANGEBASIS_HH
+#define DUNE_FUNCTIONS_FUNCTIONSPACEBASES_LAGRANGEBASIS_HH
+
+#include <dune/functions/functionspacebases/flatmultiindex.hh>
+#include <dune/functions/functionspacebases/defaultglobalbasis.hh>
+#include <dune/functions/functionspacebases/pqknodalbasis.hh>
+
+
+namespace Dune {
+namespace Functions {
+
+
+
+namespace BasisBuilder {
+
+template<std::size_t k>
+Imp::PQkNodeFactoryBuilder<k> lagrange()
+{
+  return{};
+}
+
+} // end namespace BasisBuilder
+
+
+
+/** \brief Nodal basis of a scalar k-th-order Lagrangean finite element space
+ *
+ * \note This only works for certain grids.  The following restrictions hold
+ * - If k is no larger than 2, then the grids can have any dimension
+ * - If k is larger than 3 then the grid must be two-dimensional
+ * - If k is 3, then the grid can be 3d *if* it is a simplex grid
+ *
+ * \tparam GV The GridView that the space is defined on
+ * \tparam k The order of the basis
+ */
+template<typename GV, int k, class ST = std::size_t>
+using LagrangeBasis = DefaultGlobalBasis<PQkNodeFactory<GV, k, FlatMultiIndex<ST>, ST> >;
+
+
+
+
+
+} // end namespace Functions
+} // end namespace Dune
+
+
+#endif // DUNE_FUNCTIONS_FUNCTIONSPACEBASES_PQKNODALBASIS_HH
