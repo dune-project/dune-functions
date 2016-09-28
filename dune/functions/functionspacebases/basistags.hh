d14d2c4300fbf70682e6cc535b3047495e7fc949
Carsten Gräser <graeser@mi.fu-berlin.de>
2016-09-28 22:35:58 +0200

    [doc] Improve doc of index merging strategies
    * add to doxygen groups
    * give examples

--- dune/functions/functionspacebases/basistags.hh
+++ dune/functions/functionspacebases/basistags.hh
@@ -36,50 +36,117 @@ namespace Functions {
 
 namespace BasisBuilder {
 
-
-  //! Base class for index merging strategies to simplify detection
+  /**
+   * \brief Base class for index merging strategies to simplify detection
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   */
   struct IndexMergingStrategy {};
 
   void registerIndexMergingStrategy(IndexMergingStrategy);
 
-  //! Lexicographic merging of direct children without blocking.
+  /**
+   * \brief Lexicographic merging of direct children without blocking.
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   *
+   * Example: For two children with indices
+   *
+   *   {(0,i0), (0,i1), (1,i2)} and {(0,k0), (1,k1), (2,k2)}
+   *
+   * the merged indices will be
+   *
+   *   {(0,i0), (0,i1), (1,i2), (2,k0), (3,k1), (4,k2)}
+   */
   struct FlatLexicographic
     : public IndexMergingStrategy
   {};
 
-  //! Interleaved merging of direct children without blocking.
+  /**
+   * \brief Interleaved merging of direct children without blocking.
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   *
+   * Example: For two children with indices
+   *
+   *   {(0,i0), (1,i1), (2,i2)} and {(0,i0), (1,i1), (2,i2)}
+   *
+   * the merged indices will be
+   *
+   *   {(0,i0), (2,i1), (4,i2), (1,k0), (3,k1), (5,k2)}
+   */
   struct FlatInterleaved
     : public IndexMergingStrategy
   {};
 
-  //! Lexicographic merging of direct children with blocking (i.e. creating one block per direct child).
+  /**
+   * \brief Lexicographic merging of direct children with blocking (i.e. creating one block per direct child).
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   *
+   * Example: For two children with indices
+   *
+   *   {(0,i0), (0,i1), (1,i2)} and {(0,k0), (1,k1), (2,k2)}
+   *
+   * the merged indices will be
+   *
+   *   {(0,0,i0), (0,0,i1), (0,1,i2), (1,0,k0), (1,1,k1), (1,2,k2)}
+   */
   struct BlockedLexicographic
     : public IndexMergingStrategy
   {};
 
-  //! Interleaved merging of direct children with blocking (i.e. creating blocks at the leaves containing one leaf per child each).
+  /**
+   * \brief Interleaved merging of direct children with blocking (i.e. creating blocks at the leaves containing one leaf per child each).
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   *
+   * Example: For two children with indices
+   *
+   *   {(0,i0), (1,i1), (2,i2)} and {(0,i0), (1,i1), (2,i2)}
+   *
+   * the merged indices will be
+   *
+   *   {(0,i0,0), (1,i1,0), (2,i2,0), (0,i0,1), (1,i1,1), (2,i2,1)}
+   */
   struct LeafBlockedInterleaved : public IndexMergingStrategy {};
 
 
-  //! Creates a lexicographic merging of direct children without blocking.
+  /**
+   * \brief Creates a lexicographic merging of direct children without blocking.
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   */
   constexpr FlatLexicographic flatLexicographic()
   {
     return {};
   }
 
-  //! Creates an interleaved merging of direct children without blocking.
+  /**
+   * \brief Creates an interleaved merging of direct children without blocking.
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   */
   constexpr FlatInterleaved flatInterleaved()
   {
     return {};
   }
 
-  //! Creates a lexicographic merging of direct children with blocking (i.e. creating one block per direct child).
+  /**
+   * \brief Creates a lexicographic merging of direct children with blocking (i.e. creating one block per direct child).
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   */
   constexpr BlockedLexicographic blockedLexicographic()
   {
     return {};
   }
 
-  //! Creates an interleaved merging of direct children with blocking (i.e. creating blocks at the leaves containing one leaf per child each).
+  /**
+   * \brief Creates an interleaved merging of direct children with blocking (i.e. creating blocks at the leaves containing one leaf per child each).
+   *
+   * \ingroup FunctionSpaceBasesUtilities
+   */
   constexpr LeafBlockedInterleaved leafBlockedInterleaved()
   {
     return {};
