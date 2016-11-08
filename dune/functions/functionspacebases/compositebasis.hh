b713264369e7905eee1837b3b67c259f474a4a1e
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2016-11-08 14:20:21 +0100

    Add missing includes
    `type_traits.hh` is needed for `enableIfConstructible` and `nodes.hh`
    for the `PowerBasisNode` type.

--- dune/functions/functionspacebases/compositebasis.hh
+++ dune/functions/functionspacebases/compositebasis.hh
@@ -16,6 +16,7 @@
 #include <dune/typetree/utility.hh>
 
 #include <dune/functions/common/staticforloop.hh>
+#include <dune/functions/common/type_traits.hh>
 #include <dune/functions/functionspacebases/basistags.hh>
 
 
