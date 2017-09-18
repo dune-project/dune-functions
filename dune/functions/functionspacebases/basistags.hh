7f3857d05faf5d198ec5a0d12a1b8dd929203b32
Carsten Gräser <graeser@mi.fu-berlin.de>
2017-09-18 10:43:54 +0200

    [doc] Improve documenation of IndexMergingStrategies
    Motivated by !60 I tried to improve the examples:
    * fix bug in FlatInterleaved
    * drop indices not necessary to document the Blocked*
    * Don't write indices as sets: They are maps!
    * Use tables to show the maps
    * Give indexed basis functions a name
    * Order resulting indices by new index, not by child

--- dune/functions/functionspacebases/basistags.hh
+++ dune/functions/functionspacebases/basistags.hh
@@ -50,13 +50,30 @@ namespace BasisBuilder {
    *
    * \ingroup FunctionSpaceBasesUtilities
    *
-   * Example: For two children with indices
+   * Example: For two children {f} and {g} with multi-indices (all i*,k* can be multi-indices, themselves)
    *
-   *   {(0,i0), (0,i1), (1,i2)} and {(0,k0), (1,k1), (2,k2)}
+   * function in {f}| index
+   * ---------------|------
+   *  f_0           |(0,i0)
+   *  f_1           |(0,i1)
+   *  f_2           |(1,i2)
+   *
+   * function in {g}| index
+   * ---------------|------
+   *  g_0           | (0,k0)
+   *  g_1           | (1,k1)
+   *  g_2           | (2,k2)
    *
    * the merged indices will be
    *
-   *   {(0,i0), (0,i1), (1,i2), (2,k0), (3,k1), (4,k2)}
+   * function in {f,g}| index
+   * -----------------|------
+   *  f_0             | (0,i0)
+   *  f_1             | (0,i1)
+   *  f_2             | (1,i2)
+   *  g_0             | (2,k0)
+   *  g_1             | (3,k1)
+   *  g_2             | (4,k2)
    */
   struct FlatLexicographic
     : public IndexMergingStrategy
@@ -67,13 +84,30 @@ namespace BasisBuilder {
    *
    * \ingroup FunctionSpaceBasesUtilities
    *
-   * Example: For two children with indices
+   * Example: For two children {f} and {g} with multi-indices (all i* can be multi-indices, themselves)
    *
-   *   {(0,i0), (1,i1), (2,i2)} and {(0,i0), (1,i1), (2,i2)}
+   * function in {f}| index
+   * ---------------|------
+   *  f_0           |(0,i0)
+   *  f_1           |(1,i1)
+   *  f_2           |(2,i2)
+   *
+   * function in {g}| index
+   * ---------------|------
+   *  g_0           | (0,i0)
+   *  g_1           | (1,i1)
+   *  g_2           | (2,i2)
    *
    * the merged indices will be
    *
-   *   {(0,i0), (2,i1), (4,i2), (1,k0), (3,k1), (5,k2)}
+   * function in {f,g}| index
+   * -----------------|------
+   *  f_0             | (0,i0)
+   *  g_0             | (1,i0)
+   *  f_1             | (2,i1)
+   *  g_1             | (3,i1)
+   *  f_2             | (4,i2)
+   *  g_2             | (5,i2)
    */
   struct FlatInterleaved
     : public IndexMergingStrategy
@@ -84,13 +118,30 @@ namespace BasisBuilder {
    *
    * \ingroup FunctionSpaceBasesUtilities
    *
-   * Example: For two children with indices
+   * Example: For two children {f} and {g} with multi-indices (all i*,k* can be multi-indices, themselves)
    *
-   *   {(0,i0), (0,i1), (1,i2)} and {(0,k0), (1,k1), (2,k2)}
+   * function in {f}| index
+   * ---------------|------
+   *  f_0           |(i0)
+   *  f_1           |(i1)
+   *  f_2           |(i2)
+   *
+   * function in {g}| index
+   * ---------------|------
+   *  g_0           | (k0)
+   *  g_1           | (k1)
+   *  g_2           | (k2)
    *
    * the merged indices will be
    *
-   *   {(0,0,i0), (0,0,i1), (0,1,i2), (1,0,k0), (1,1,k1), (1,2,k2)}
+   * function in {f,g}| index
+   * -----------------|------
+   *  f_0             | (0,i0)
+   *  f_1             | (0,i1)
+   *  f_2             | (0,i2)
+   *  g_0             | (1,k0)
+   *  g_1             | (1,k1)
+   *  g_2             | (1,k2)
    */
   struct BlockedLexicographic
     : public IndexMergingStrategy
@@ -101,13 +152,30 @@ namespace BasisBuilder {
    *
    * \ingroup FunctionSpaceBasesUtilities
    *
-   * Example: For two children with indices
+   * Example: For two children {f} and {g} with multi-indices (all i* can be multi-indices, themselves)
    *
-   *   {(0,i0), (1,i1), (2,i2)} and {(0,i0), (1,i1), (2,i2)}
+   * function in {f}| index
+   * ---------------|------
+   *  f_0           |(i0)
+   *  f_1           |(i1)
+   *  f_2           |(i2)
+   *
+   * function in {g}| index
+   * ---------------|------
+   *  g_0           | (i0)
+   *  g_1           | (i1)
+   *  g_2           | (i2)
    *
    * the merged indices will be
    *
-   *   {(0,i0,0), (1,i1,0), (2,i2,0), (0,i0,1), (1,i1,1), (2,i2,1)}
+   * function in {f,g}| index
+   * -----------------|------
+   *  f_0             | (i0,0)
+   *  g_0             | (i0,1)
+   *  f_1             | (i1,0)
+   *  g_1             | (i1,1)
+   *  f_2             | (i2,0)
+   *  g_2             | (i2,1)
    */
   struct LeafBlockedInterleaved : public IndexMergingStrategy {};
 
