ebc4c5596bcd8a1b4a1ed7c6210bf5f044184b84
Oliver Sander <oliver.sander@tu-dresden.de>
2018-02-28 12:26:20 +0100

    A unit test for the BrezziDouglasMariniBasis class

---
+++ dune/functions/functionspacebases/test/brezzidouglasmarinibasistest.cc
@@ -0,0 +1,51 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+
+#include <dune/common/exceptions.hh>
+#include <dune/common/parallel/mpihelper.hh>
+
+#include <dune/grid/yaspgrid.hh>
+
+#include <dune/functions/functionspacebases/brezzidouglasmarinibasis.hh>
+
+#include <dune/functions/functionspacebases/test/basistest.hh>
+
+using namespace Dune;
+
+int main (int argc, char* argv[])
+{
+  MPIHelper::instance(argc, argv);
+
+  TestSuite test;
+
+
+
+  // Generate grid for testing
+  const int dim = 2;
+  typedef YaspGrid<dim> GridType;
+  FieldVector<double,dim> l(1);
+  std::array<int,dim> elements = {{10, 10}};
+  GridType grid(l,elements);
+
+
+
+  // check RaviartThomasBasis created 'manually'
+  {
+    typedef GridType::LeafGridView GridView;
+    const GridView& gridView = grid.leafGridView();
+    Functions::BrezziDouglasMariniBasis<GridView,1,GeometryType::BasicType::cube> basis(gridView);
+    test.subTest(checkBasis(basis));
+  }
+
+
+
+  // check RaviartThomasBasis created using basis builder mechanism
+  {
+    using namespace Functions::BasisBuilder;
+    auto basis = makeBasis(grid.leafGridView(), bdm<1, GeometryType::BasicType::cube>());
+    test.subTest(checkBasis(basis));
+  }
+
+  return test.exit();
+}
