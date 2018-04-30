1e0c8b9adde5ad20be8bfccd87bf935f6e3e1f50
Oliver Sander <oliver.sander@tu-dresden.de>
2018-04-30 21:56:14 +0200

    Test BrezziDouglasMariniBasis with a UGGrid object
    This is worth testing because UGGrid does not have a compile-time-fixed
    element type.

--- dune/functions/functionspacebases/test/brezzidouglasmarinibasistest.cc
+++ dune/functions/functionspacebases/test/brezzidouglasmarinibasistest.cc
@@ -5,6 +5,7 @@
 #include <dune/common/exceptions.hh>
 #include <dune/common/parallel/mpihelper.hh>
 
+#include <dune/grid/uggrid.hh>
 #include <dune/grid/yaspgrid.hh>
 
 #include <dune/functions/functionspacebases/brezzidouglasmarinibasis.hh>
@@ -30,7 +31,7 @@ int main (int argc, char* argv[])
 
 
 
-  // check RaviartThomasBasis created 'manually'
+  // check BrezziDouglasMariniBasis created 'manually'
   {
     typedef GridType::LeafGridView GridView;
     const GridView& gridView = grid.leafGridView();
@@ -40,10 +41,19 @@ int main (int argc, char* argv[])
 
 
 
-  // check RaviartThomasBasis created using basis builder mechanism
+  // check BrezziDouglasMariniBasis created using basis factory mechanism
   {
     using namespace Functions::BasisFactory;
     auto basis = makeBasis(grid.leafGridView(), brezziDouglasMarini<1, GeometryType::BasicType::cube>());
+    test.subTest(checkBasis(basis));
+  }
+
+
+  // check BrezziDouglasMariniBasis on a grid without a compile-time-fixed element type
+  {
+    using Grid = UGGrid<dim>;
+    std::shared_ptr<Grid> grid = StructuredGridFactory<Grid>::createCubeGrid({0.0,0.0}, l, {10,10});
+    Functions::BrezziDouglasMariniBasis<Grid::LeafGridView,1,GeometryType::BasicType::cube> basis(grid->leafGridView());
     test.subTest(checkBasis(basis));
   }
 
