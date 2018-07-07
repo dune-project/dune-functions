75d573fa4445557541b60c0470b898325519d2d7
Carsten Gräser <graeser@mi.fu-berlin.de>
2018-07-07 02:09:53 +0200

    Add different GeometryTypeProviders
    These allow to map an entity to its geometry type. Depending on the
    selected provider and grid view, this is encoded as GeometryType or
    StaticGeometryType. There are several implementations:
    * MixedGeometryTypeProvider will always act like a mixed element
      grid and simply forward the dyanamic GeometryType. You'd normally
      not want to use this one.
    * AutoGeometryTypeProvider will use a StaticGeometryType if the grid
      satisfies hasSingleGeometryType(). Otherwise GeometryType is used.
      This is the default choise which should work always.
    * SimplexGeometryTypeProvider and CubeGeometryTypeProvider will
      always provide the corresponding StaticGeometryType. This can be used
      to avoid the costly polymorphic interface if your grid supports mixed
      elements, but you know, that there are only simplicies or cubes.

---
+++ dune/functions/functionspacebases/geometrytypeprovider.hh
@@ -0,0 +1,83 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_FUNCTIONSPACEBASES_GEOMETRYTYPEPROVIDER_HH
+#define DUNE_FUNCTIONS_FUNCTIONSPACEBASES_GEOMETRYTYPEPROVIDER_HH
+
+#include <dune/common/std/type_traits.hh>
+#include <dune/common/indices.hh>
+#include <dune/common/keywords.hh>
+#include <dune/geometry/type.hh>
+
+#include <dune/functions/common/staticgeometrytype.hh>
+
+
+
+namespace Dune {
+namespace Functions {
+
+
+
+struct MixedGeometryTypeProvider
+{
+  template<class GridView, class Entity>
+  static constexpr auto type(const Entity& entity)
+  {
+    return entity.type();
+  }
+
+  template<class GridView>
+  using Type = GeometryType;
+};
+
+struct SimplexGeometryTypeProvider
+{
+  template<class GridView, class Entity>
+  static constexpr auto type(const Entity& entity)
+  {
+    return StaticGeometryTypes::Simplex<GridView::dimension>();
+  }
+
+  template<class GridView>
+  using Type = StaticGeometryTypes::Simplex<GridView::dimension>;
+};
+
+struct CubeGeometryTypeProvider
+{
+  template<class GridView, class Entity>
+  static constexpr auto type(const Entity& entity)
+  {
+    return StaticGeometryTypes::Cube<GridView::dimension>();
+  }
+
+  template<class GridView>
+  using Type = StaticGeometryTypes::Cube<GridView::dimension>;
+};
+
+struct AutoGeometryTypeProvider
+{
+  template<class GridView, class Entity,
+    std::enable_if_t<Dune::Capabilities::hasSingleGeometryType<typename GridView::Grid>::v, int> = 0>
+  static constexpr auto type(const Entity& entity)
+  {
+    return StaticGeometryType<Dune::Capabilities::hasSingleGeometryType<typename GridView::Grid>::topologyId, GridView::dimension, false>{};
+  }
+
+  template<class GridView, class Entity,
+    std::enable_if_t<not Dune::Capabilities::hasSingleGeometryType<typename GridView::Grid>::v, int> = 0>
+  static constexpr auto type(const Entity& entity)
+  {
+    return entity.type();
+  }
+
+  template<class GridView>
+  using Type = decltype(type<GridView>(std::declval<typename GridView::template Codim<0>::Entity>()));
+};
+
+
+
+} // namespace Functions
+} // namespace Dune
+
+
+
+#endif // DUNE_FUNCTIONS_FUNCTIONSPACEBASES_GEOMETRYTYPEPROVIDER_HH
