d105bce2fbb7293d91dceb3144a82c85444da816
Oliver Sander <oliver.sander@tu-dresden.de>
2018-08-16 06:59:03 +0000

    [!183] Catch polymorphic exceptions by reference
    Merge branch 'catch-polymorphic-exception-by-reference' into 'master'
    
    g++ 8 warns about this.
    
    See merge request [staging/dune-functions!183]
    
      [staging/dune-functions!183]: gitlab.dune-project.org/staging/dune-functions/merge_requests/183
    
    
    (cherry picked from commit cc1312ad29b1d757caf5eec049985f283b9d4a9e)
    
    23fe5770 Catch polymorphic exceptions by reference

--- examples/stokes-taylorhood.cc
+++ examples/stokes-taylorhood.cc
@@ -524,6 +524,6 @@ int main (int argc, char *argv[]) try
 
  }
 // Error handling
- catch (Exception e) {
-    std::cout << e << std::endl;
+ catch (Exception& e) {
+    std::cout << e.what() << std::endl;
  }
