8887a026b5d1041492c64d8fd28a2a4ff6894be3
Jakub Both <jakub.both@uib.no>
2020-12-13 00:21:37 +0100

    Use a more standard structure for the test.

--- dune/functions/functionspacebases/test/constantinterpolationtest.cc
+++ dune/functions/functionspacebases/test/constantinterpolationtest.cc
@@ -47,56 +47,61 @@ int main (int argc, char *argv[]) try
 {
   // Set up MPI, if available
   MPIHelper::instance(argc, argv);
+  bool passed = true;
 
-  ///////////////////////////////////
-  //   Generate a simple grid
-  ///////////////////////////////////
+  // Generate grid for testing
   const int dim = 2;
   using Grid = Dune::YaspGrid<dim>;
   Dune::FieldVector<double,dim> l(1.0);
   std::array<int,dim> elements = {{2, 2}};
   auto gridPtr = std::make_unique<Grid>(l, elements);
-
   auto& grid = *gridPtr;
   grid.globalRefine(2);
-
   auto gridView = grid.leafGridView();
-  using GridView = decltype(gridView);
 
   /////////////////////////////////////////////////////////
   //   Choose a finite element space
   /////////////////////////////////////////////////////////
   using namespace Functions::BasisBuilder;
-  using namespace TypeTree::Indices;
 
-  auto vectorialLagrangeBasis = makeBasis(gridView, power<2>(lagrange<1>(), flatInterleaved()));
-  auto raviartThomasBasis = makeBasis(gridView, raviartThomas<0>());
-
-  // Test interpolation of constant function for scalar Lagrange basis
+  // Test interpolation of constant function for
+  // ... scalar Lagrange basis
   using ScalarRange = double;
   auto scalarValuedFunction = [](const auto& x) { return 2;};
 
   auto scalarLagrangeBasis = makeBasis(gridView, lagrange<1>());
-  auto scalarLagrangeBasisTest = interpolateAndCompareFunctions<ScalarRange>(scalarLagrangeBasis, scalarValuedFunction, gridView);
-  std::cout << "Constant interpolation for scalar Lagrange Basis " << (scalarLagrangeBasisTest? "" : "NOT ") << "successful." << std::endl;
-  assert(scalarLagrangeBasisTest == true);
+  auto passedScalarLagrangeBasisTest = interpolateAndCompareFunctions<ScalarRange>(scalarLagrangeBasis, scalarValuedFunction, gridView);
+  std::cout << "Constant interpolation for scalar Lagrange Basis " << (passedScalarLagrangeBasisTest? "" : "NOT ") << "successful." << std::endl;
+  passed = passed and passedScalarLagrangeBasisTest;
 
-  // Test interpolation of constant function for vectorial bases
+  // Test interpolation of constant vector valued function for ...
   using VectorRange = FieldVector<ScalarRange, 2>;
   auto vectorValuedFunction = [](const auto& x) { return FieldVector<double,2>{1, 2};};
 
-  // ... Lagrange basis
-  auto vectorialLagrangeBasisTest = interpolateAndCompareFunctions<VectorRange>(vectorialLagrangeBasis, vectorValuedFunction, gridView);
-  std::cout << "Constant interpolation for vectorial Lagrange basis " << (vectorialLagrangeBasisTest? "" : "NOT ") << "successful." << std::endl;
-  assert(raviartThomasTest == true);
+  // ... power Lagrange basis
+  auto vectorialLagrangeBasis = makeBasis(gridView, power<2>(lagrange<1>(), flatInterleaved()));
+  auto passedVectorialLagrangeBasisTest = interpolateAndCompareFunctions<VectorRange>(vectorialLagrangeBasis, vectorValuedFunction, gridView);
+  std::cout << "Constant interpolation for vectorial Lagrange basis " << (passedVectorialLagrangeBasisTest? "" : "NOT ") << "successful." << std::endl;
+  passed = passed and passedVectorialLagrangeBasisTest;
 
   // ... Raviart-Thomas basis
-  auto raviartThomasBasisTest = interpolateAndCompareFunctions<VectorRange>(raviartThomasBasis, vectorValuedFunction, gridView);
-  std::cout << "Constant interpolation for Raviart-Thomas basis " << (raviartThomasBasisTest? "" : "NOT ") << "successful." << std::endl;
-  assert(raviartThomasTest == true);
+  auto raviartThomasBasis = makeBasis(gridView, raviartThomas<0>());
+  auto passedRaviartThomasBasisTest = interpolateAndCompareFunctions<VectorRange>(raviartThomasBasis, vectorValuedFunction, gridView);
+  std::cout << "Constant interpolation for Raviart-Thomas basis " << (passedRaviartThomasBasisTest? "" : "NOT ") << "successful." << std::endl;
+  passed = passed and passedRaviartThomasBasisTest;
 
- }
-// Error handling
- catch (Exception& e) {
-    std::cout << e.what() << std::endl;
- }
+  if (passed)
+    std::cout << "All tests passed" << std::endl;
+
+  return passed ? 0 : 1;
+
+} catch ( Dune::Exception &e )
+{
+  std::cerr << "Dune reported error: " << e << std::endl;
+  return 1;
+}
+catch(...)
+{
+  std::cerr << "Unknown exception thrown!" << std::endl;
+  return 1;
+}
