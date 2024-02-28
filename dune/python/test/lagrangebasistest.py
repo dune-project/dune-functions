64ffe7565cdc6bce6b3d826776314f61de991003
Oliver Sander <oliver.sander@tu-dresden.de>
2024-02-28 09:40:22 +0100

    Further cleanup of lagrangebasistest.py

--- dune/python/test/lagrangebasistest.py
+++ dune/python/test/lagrangebasistest.py
@@ -1,5 +1,5 @@
 import dune.grid
-import dune.functions
+import dune.functions as functions
 
 import basistest   # The general test suite for function space bases
 
@@ -7,27 +7,23 @@ import basistest   # The general test suite for function space bases
 # Test all currently supported global bases in dimension `dimension`.
 # Only the size of the global bases are tested.
 def test(dimension):
-    lowerLeft = []
-    upperRight = []
-    elements = []
-    for i in range(dimension):
-        lowerLeft.append(-1)
-        upperRight.append(1)
-        elements.append(3)
+    lowerLeft = [-1] * dimension
+    upperRight = [1] * dimension
+    elements = [3] * dimension
 
     grid = dune.grid.structuredGrid(lowerLeft,upperRight,elements)
 
     # Test a first-order Lagrange basis
-    basisLagrange1 = dune.functions.defaultGlobalBasis(grid, dune.functions.Lagrange(order=1))
-    basistest.checkBasis(basisLagrange1)
+    basis1 = functions.defaultGlobalBasis(grid, functions.Lagrange(order=1))
+    basistest.checkBasis(basis1)
 
     # Test a second-order Lagrange basis
-    basisLagrange2 = dune.functions.defaultGlobalBasis(grid, dune.functions.Lagrange(order=2))
-    basistest.checkBasis(basisLagrange2)
+    basis2 = functions.defaultGlobalBasis(grid, functions.Lagrange(order=2))
+    basistest.checkBasis(basis2)
 
     # Test a vector-value Lagrange basis
-    basisPower = dune.functions.defaultGlobalBasis(grid, dune.functions.Power(dune.functions.Lagrange(order=1),exponent=dimension))
-    assert(len(basisPower) == len(basisLagrange1)*dimension)
+    basisPower = functions.defaultGlobalBasis(grid, functions.Power(functions.Lagrange(order=1),exponent=dimension))
+    basistest.checkBasis(basisPower)
 
     # Test a vector-value Lagrange basis with different orders
     basisComposite = dune.functions.defaultGlobalBasis(grid, dune.functions.Composite(dune.functions.Power(dune.functions.Lagrange(order=2),exponent=dimension),
