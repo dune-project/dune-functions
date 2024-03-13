eb77ddeb1f17e06a3950ec9dae87a2dc29cc1a89
Carsten Gräser <graeser@math.fau.de>
2024-03-13 15:58:24 +0100

    [python][test] Check constructor of DiscreteFunction

--- dune/python/test/discretefunction.py
+++ dune/python/test/discretefunction.py
@@ -12,10 +12,16 @@ basis = dune.functions.defaultGlobalBasis(grid, dune.functions.Lagrange(order=1)
 coeff = np.zeros( len(basis) )
 
 # discrete function over the grid
-discreteFunction = basis.asFunction(coeff)
+f = basis.asFunction(coeff)
 
 # extract the original grid
-grid2 = discreteFunction.grid
+grid2 = f.grid
+
+# deduce type of discrete function
+DiscreteFunction = type(f)
+
+# check constructor of DiscreteFunction
+f2 = DiscreteFunction(basis, coeff)
 
 # check if there are still 100 squares
 assert( grid2.size(0) == 100 )
