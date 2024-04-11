d9e182505a57ce91ebcb06bec54fe9a10bf0c09d
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-04-12 00:42:24 +0200

    Add a vector factory based on the container descriptors

---
+++ dune/functions/backends/istlvectorfactory.hh
@@ -0,0 +1,116 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_BACKENDS_ISTL_VECTORFACTORY_HH
+#define DUNE_FUNCTIONS_BACKENDS_ISTL_VECTORFACTORY_HH
+
+#include <array>
+#include <cassert>
+#include <functional>
+#include <type_traits>
+#include <vector>
+
+#include <dune/common/fvector.hh>
+#include <dune/common/filledarray.hh>
+#include <dune/common/tuplevector.hh>
+
+#include <dune/functions/functionspacebases/containerdescriptors.hh>
+
+#include <dune/istl/bvector.hh>
+#include <dune/istl/multitypeblockvector.hh>
+
+namespace Dune::Functions {
+namespace ContainerDescriptors {
+
+template<class T>
+struct ISTLVectorFactory
+{
+  void operator() (const Unknown& tree) const
+  {
+    DUNE_THROW(Dune::NotImplemented, "Cannot create a vector. The container descriptor is unknown.");
+  }
+
+  template<class... V>
+  auto operator() (const Tuple<V...>& tree) const
+  {
+    return unpackIntegerSequence([&](auto... ii) {
+      return Dune::MultiTypeBlockVector<decltype((*this)(tree[ii]))...>{(*this)(tree[ii])...};
+    }, std::make_index_sequence<sizeof...(V)>());
+  }
+
+  template<class V, std::size_t n>
+  auto operator() (const Array<V,n>& tree) const
+  {
+    return unpackIntegerSequence([&](auto... ii) {
+      return Dune::BlockVector{(*this)(tree[ii])...};
+    }, std::make_index_sequence<n>());
+  }
+
+  template<class V>
+  auto operator() (const Vector<V>& tree) const
+  {
+    using W = decltype((*this)(tree[0]));
+    Dune::BlockVector<W> result(tree.size());
+    for (std::size_t i = 0; i < tree.size(); ++i)
+      result[i] = (*this)(tree[i]);
+    return result;
+  }
+
+  template<class V, std::size_t n>
+  auto operator() (const UniformArray<V,n>& tree) const
+  {
+    auto node = (*this)(tree[0]);
+    return unpackIntegerSequence([&](auto... ii) {
+      return Dune::BlockVector{((void)(ii),node)...};
+    }, std::make_index_sequence<n>());
+  }
+
+  template<class V>
+  auto operator() (const UniformVector<V>& tree) const
+  {
+    auto node = (*this)(tree[0]);
+    using W = decltype(node);
+    Dune::BlockVector<W> result(tree.size());
+    for (std::size_t i = 0; i < tree.size(); ++i)
+      result[i] = node;
+    return result;
+  }
+
+  // final overloads that break the recursion:
+
+  auto operator() (const Value& tree) const
+  {
+    return T(0);
+  }
+
+  template<std::size_t n>
+  auto operator() (const UniformArray<Value,n>& tree) const
+  {
+    return Dune::FieldVector<T,n>(0);
+  }
+
+  auto operator() (const UniformVector<Value>& tree) const
+  {
+    return Dune::BlockVector<T>(tree.size());
+  }
+
+  template<std::size_t n>
+  auto operator() (const UniformVector<UniformArray<Value,n>>& tree) const
+  {
+    return Dune::BlockVector<Dune::FieldVector<T,n>>(tree.size());
+  }
+};
+
+} // end namespace ContainerDescriptors
+
+
+// Construct an istl vector type compatible with the container descriptor
+template<class T = double, class ContainerDescriptor>
+auto istlVectorFactory (const ContainerDescriptor& tree)
+{
+  auto factory = ContainerDescriptors::ISTLVectorFactory<T>{};
+  return factory(tree);
+}
+
+} // end namespace Dune::Functions
+
+#endif // DUNE_FUNCTIONS_BACKENDS_ISTL_VECTORFACTORY_HH
