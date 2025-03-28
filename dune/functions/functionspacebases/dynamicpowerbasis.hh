15fa760aeea834c1ca94b807f4493d3e512bd25c
Oliver Sander <oliver.sander@tu-dresden.de>
2025-03-28 13:43:23 +0100

    Inform users of pending change of default index merging strategies
    Using the methods `power` or `composite` to construct function space
    basis trees without an explicit index merging strategy argument
    will now issue a warning. This warning informs that the default
    index merging strategy will change after the 2.11 release.
    More specifically, it will change from `BlockedLexicographic`
    to `FlatLexicographic` for the `composite` method, and from
    `BlockedInterleaved` to `FlatInterleaved` for the `power` method.
    The consequence is that default indexing is always flat,
    which is the most common form of indexing.
    
    To ensure a smooth transition we suggest to not rely on default
    index merging strategies until 2.11 has been released.

--- dune/functions/functionspacebases/dynamicpowerbasis.hh
+++ dune/functions/functionspacebases/dynamicpowerbasis.hh
@@ -425,6 +425,11 @@ auto power(ChildPreBasisFactory&& childPreBasisFactory, std::size_t k, const Ind
  * This overload will select the BasisFactory::BlockedInterleaved strategy.
  */
 template<class ChildPreBasisFactory>
+[[deprecated("Using the method `power` without an explicit index merging strategy"
+  " will change its meaning after the release of dune-functions 2.11."
+  " Previously, the default merging strategy was `BlockedInterleaved`,"
+  " but this will change to `FlatInterleaved`."
+  " Change the call to `power(..., blockedInterleaved())` to retain the old behavior.")]]
 auto power(ChildPreBasisFactory&& childPreBasisFactory, std::size_t k)
 {
   return [childPreBasisFactory,k](const auto& gridView) {
